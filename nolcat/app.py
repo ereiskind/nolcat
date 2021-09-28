adc3aa4c7ffc31f49532e5180aa497f9e33c270d
Elissa_Reiskind <ereiskind@fsu.edu>
2021-09-28 11:01:06 -0400

    Create inner function for homepage

--- nolcat/app.py
+++ nolcat/app.py
@@ -4,4 +4,8 @@ def create_app():
     """A factory pattern for instantiating Flask web apps."""
     app = Flask(__name__)
 
+    @app.route('/')
+    def homepage():
+        return "This is the homepage"
+
     return app
