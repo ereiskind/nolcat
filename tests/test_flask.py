0119812171645ddb70556615a3a50c44562a854d
Elissa_Reiskind <ereiskind@fsu.edu>
2021-09-28 11:07:33 -0400

    Create fixture for Flask web app client

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,3 +1,16 @@
 """This module contains the tests for the Flask web app."""
 # https://flask.palletsprojects.com/en/2.0.x/testing/
 # https://flask.palletsprojects.com/en/2.0.x/tutorial/tests/
+
+import pytest
+
+from nolcat.app import create_app
+
+
+@pytest.fixture
+def flask_client():
+    """Creates an instance of the Flask web app for testing."""
+    app = create_app()
+    app.testing = True  # Lets exceptions come through to test client
+    with app.test_client() as client:
+        yield client
