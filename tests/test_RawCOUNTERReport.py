54352f38726176780e44223fdb5df9ab4d09f46b
Elissa_Reiskind <ereiskind@fsu.edu>
2021-09-29 13:22:10 -0400

    Read sample data CSVs into fixture function

--- tests/test_RawCOUNTERReport.py
+++ tests/test_RawCOUNTERReport.py
@@ -1,11 +1,15 @@
 """Test methods in RawCOUNTERReport."""
+import os
+from pathlib import Path
 import pytest
 
 
 @pytest.fixture
 def sample_R4_dataframe():
     """Reads the sample CSVs of reformatted R4 data into a single dataframe using the same expression as in the `determine_if_resources_match` function."""
-    pass
+    R4_reports = []
+    for file in os.listdir(Path('.', 'tests', 'data')):  # The paths are based off the project root so pytest can be invoked through the Python interpreter at the project root
+        R4_reports.append(Path('.', 'tests', 'data', file))
 
 
 def test_perform_deduplication_matching(sample_R4_dataframe):
