ed96d44dc9a962bfcdddacbfaa955e8d51757e27
Elissa_Reiskind <ereiskind@fsu.edu>
2021-09-30 12:47:03 -0400

    Update docstrings based on return statement format

--- nolcat/raw_COUNTER_report.py
+++ nolcat/raw_COUNTER_report.py
@@ -43,7 +43,11 @@ class RawCOUNTERReport:
             normalized_resource_data (dataframe, optional): the database's normalized list of resources; has a value of None during the initial construction of that list
         
         Returns:
-            [type]: [description]
+            tuple: the variables matched_records and matches_to_manually_confirm, described in the note, in a tuple for unpacking through multiple assignment
+        
+        Note:
+            matched_records: a set of tuples containing the record index values of matched records
+            matches_to_manually_confirm: a dict with keys that are tuples containing the metadata for two resources and values that are a list of tuples containing the record index values of record matches with one of the records corresponding to each of the resources in the tuple
         """
         logging.info(f"The new COUNTER report:\n{self}")
         if normalized_resource_data:
--- tests/test_RawCOUNTERReport.py
+++ tests/test_RawCOUNTERReport.py
@@ -43,7 +43,7 @@ def sample_R4_dataframe():
 
 
 def test_perform_deduplication_matching(sample_R4_dataframe):
-    """Uses the `sample_R4_dataframe` as a single dataframe argument to test the `perform_deduplication_matching` function."""  #ToDo: Write a better docstring when the format of the return value is set
+    """Uses the `sample_R4_dataframe` as a single dataframe argument to test that the `perform_deduplication_matching` function returns the data representing resource matches both confirmed and needing confirmation."""
     R4_test_data = RawCOUNTERReport(sample_R4_dataframe)
     R4_test_data.perform_deduplication_matching()  #ToDo: Assert this to its return value when that's known
     assert True
