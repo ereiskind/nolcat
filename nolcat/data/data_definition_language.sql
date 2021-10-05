344d4e3bba96e7de0c0b3196d14fac329fbd136b
Elissa_Reiskind <ereiskind@fsu.edu>
2021-10-05 14:54:09 -0400

    Create relation annualUsageCollectionTracking

--- nolcat/data/data_definition_language.sql
+++ nolcat/data/data_definition_language.sql
@@ -70,3 +70,84 @@ CREATE TABLE statisticsSourceNotes (
         ON UPDATE restrict
         ON DELETE restrict
 );
+
+
+CREATE TABLE annualUsageCollectionTracking (
+    AUCT_Statistics_Source INT NOT NULL,
+    AUCT_Fiscal_Year INT NOT NULL,
+    Usage_Is_Being_Collected BOOLEAN NOT NULL,
+    Manual_Collection_Required BOOLEAN,
+    Collection_Via_Email BOOLEAN,
+    Is_COUNTER_Compliant BOOLEAN,
+    Collection_Status ENUM(
+        'N/A: Paid by Law',
+        'N/A: Paid by Med',
+        'N/A: Paid by Music',
+        'N/A: Open access',
+        'N/A: Other (see notes)',
+        'Collection not started',
+        'Collection in process (see notes)',
+        'Collection issues requiring resolution',
+        'Collection complete',
+        'Usage not provided',
+        'No usage to report'
+    ),
+    Usage_File_Path VARCHAR(150),
+    Notes TEXT,
+    PRIMARY KEY (AUCT_Statistics_Source, AUCT_Fiscal_Year),
+    CHECK (-- A constraint to ensure only valid situations are represented
+        (-- Usage collection not required
+            Usage_Is_Being_Collected=false AND
+            Manual_Collection_Required=null AND
+            Collection_Via_Email=null AND
+            Is_COUNTER_Compliant=null AND
+            (
+                Collection_Status='N/A: Paid by Law' OR
+                Collection_Status='N/A: Paid by Med' OR
+                Collection_Status='N/A: Paid by Music' OR
+                Collection_Status='N/A: Open access' OR
+                Collection_Status='N/A: Other (see notes)'
+            )
+        )
+        OR
+        (-- Manual usage collection, including COUNTER R4
+            Usage_Is_Being_Collected=true AND
+            (
+                Collection_Via_Email=true OR
+                Collection_Via_Email=false
+            ) AND
+            Manual_Collection_Required=true AND
+            (
+                Collection_Status='Collection not started' OR
+                Collection_Status='Collection in process (see notes)' OR
+                Collection_Status='Collection issues requiring resolution' OR
+                Collection_Status='Collection complete' OR
+                Collection_Status='Usage not provided' OR
+                Collection_Status='No usage to report'
+            )
+        )
+        OR
+        (-- R5 SUSHI collection
+            Usage_Is_Being_Collected=true AND
+            Manual_Collection_Required=false AND
+            Collection_Via_Email=null AND
+            Is_COUNTER_Compliant=true AND
+            (
+                Collection_Status='Collection not started' OR
+                Collection_Status='Collection issues requiring resolution' OR
+                Collection_Status='Collection complete' OR
+                Collection_Status='No usage to report'
+            )
+        )
+    )
+    INDEX statisticsSources_FK_INDX (AUCT_Statistics_Source,
+    CONSTRAINT statisticsSources_FK_annualUsageCollectionTracking FOREIGN KEY statisticsSources_FK_INDX (AUCT_Statistics_Source)
+        REFERENCES statisticsSources(Statistics_Source_ID)
+        ON UPDATE restrict
+        ON DELETE restrict,
+    INDEX fiscalYears_FK_INDX (AUCT_Fiscal_Year),
+    CONSTRAINT fiscalYears_FK_annualUsageCollectionTracking FOREIGN KEY fiscalYears_FK_INDX (AUCT_Fiscal_Year)
+        REFERENCES fiscalYears(Fiscal_Year_ID)
+        ON UPDATE restrict
+        ON DELETE restrict
+);
