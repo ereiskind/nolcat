0ed098dae1294fccac3dcd15531203fc7d2b997e
Elissa_Reiskind <ereiskind@fsu.edu>
2021-10-12 13:42:29 -0400

    Outline recreation of R4 RawCOUNTERReport fixture

--- tests/test_RawCOUNTERReport.py
+++ tests/test_RawCOUNTERReport.py
@@ -4,48 +4,39 @@ from pathlib import Path
 import pytest
 import pandas as pd
 from selenium.webdriver.common.keys import Keys
-from werkzeug.datastructures import *
+
 from nolcat.raw_COUNTER_report import RawCOUNTERReport
+from nolcat.app import Chrome_browser_driver
 
 
 @pytest.fixture
-def sample_R4_RawCOUNTERReport():
-    """Uses the RawCOUNTERReport constructor to create a RawCOUNTERReport object from reformatted R4 COUNTER reports."""
-    R4_reports = MultiDict()
-    for file in os.listdir(Path('.', 'tests', 'bin', 'OpenRefine_exports')):  # The paths are based off the project root so pytest can be invoked through the Python interpreter at the project root
-        R4_reports.add(
-            'R4_files',
-            FileStorage(
-                stream=open(
-                    Path('.', 'tests', 'bin', 'OpenRefine_exports', file),
-                    encoding='unicode_escape',
-                ),
-                name='R4_files',
-                headers={
-                    'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                    'Content-Encoding': 'utf-8',
-                    'mode': 'b',
-                }
-            )
-        )
+def sample_R4_form_result():
+    """Uses Selenium to pass reformatted R4 COUNTER reports into the form at the end of route upload_historical_COUNTER_usage, simulating one of the possible arguments for the RawCOUNTERReport constructor."""
+    driver = Chrome_browser_driver()
+    #ToDo: Go to url_for('upload_historical_COUNTER_usage')
+    #ToDo: Find form element with name 'R4_files'
+    #ToDo: for file in os.listdir(Path('.', 'tests', 'bin', 'OpenRefine_exports')):  # The paths are based off the project root so pytest can be invoked through the Python interpreter at the project root
+        #ToDo: Add Path('.', 'tests', 'bin', 'OpenRefine_exports', file) to the list of files selected in field 'R4_files'
+    #ToDo: Select type="submit" button
+    #ToDo: R4_reports = value retruend from the form (`request.files` in flask routes)
     yield R4_reports
 
 
-def test_RawCOUNTERReport_fixture_creation(sample_R4_RawCOUNTERReport):
-    """Confirms that the RawCOUNTERReport object `sample_R4_RawCOUNTERReport`, which is used as a fixture, was instantiated correctly."""
+def test_RawCOUNTERReport_R4_constructor(sample_R4_form_result):
+    """Confirms that constructor for RawCOUNTERReport that takes in reformatted R4 reports is working correctly."""
     pass
 
 
-def test_perform_deduplication_matching(sample_R4_RawCOUNTERReport):
-    """Tests that the `perform_deduplication_matching` method returns the data representing resource matches both confirmed and needing confirmation with the `sample_R4_RawCOUNTERReport` instance as the sole argument."""
-    assert sample_R4_RawCOUNTERReport.perform_deduplication_matching() == None  #ToDo: Determine what new value is
+def test_perform_deduplication_matching(sample_R4_form_result):
+    """Tests that the `perform_deduplication_matching` method returns the data representing resource matches both confirmed and needing confirmation with the object instantiated from `sample_R4_form_result` as the sole argument."""
+    assert sample_R4_form_result.perform_deduplication_matching() == None  #ToDo: Determine what new value is
 
 
-def test_harvest_SUSHI_report(sample_R4_RawCOUNTERReport):
+def test_harvest_SUSHI_report(sample_R4_form_result):
     #ToDo: Write a docstring when the format of the return value is set
     pass
     
 
-def test_load_data_into_database(sample_R4_RawCOUNTERReport):
+def test_load_data_into_database(sample_R4_form_result):
     #ToDo: Write a docstring when the format of the return value is set
     pass
