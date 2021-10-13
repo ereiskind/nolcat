ccdc1f699701659e69bfa44d55936191aae048d5
Elissa_Reiskind <ereiskind@fsu.edu>
2021-10-13 12:01:46 -0400

    Add link to Selenium tutorial

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,6 +1,7 @@
 """This module contains the tests for the Flask web app."""
 # https://flask.palletsprojects.com/en/2.0.x/testing/
 # https://flask.palletsprojects.com/en/2.0.x/tutorial/tests/
+# https://scotch.io/tutorials/test-a-flask-app-with-selenium-webdriver-part-1
 
 from pathlib import Path
 import os
