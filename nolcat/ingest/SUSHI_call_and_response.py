2c20137376b497b4cda0b5bf8129d3bf43a20670
Elissa_Reiskind <ereiskind@fsu.edu>
2021-10-22 12:21:45 -0400

    Create SUSHICallAndResponse class

---
+++ nolcat/ingest/SUSHI_call_and_response.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format="SUSHICallAndResponse - - [%(asctime)s] %(message)s")
+
+
+class SUSHICallAndResponse:
+    #ToDo: Create class that makes SUSHI API calls with error checking for both the API calls and the SUSHI content
+    pass
