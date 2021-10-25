f590a1b5d21ba67406854aad28c5316b55c95359
Elissa_Reiskind <ereiskind@fsu.edu>
2021-10-25 09:41:04 -0400

    Correct timestamp functions

--- tests/run_nolcat_tests.py
+++ tests/run_nolcat_tests.py
@@ -24,7 +24,7 @@ RUN git clone https://github.com/ereiskind/nolcat.git -b {branch_name} ./nolcat/
 RUN pip install --no-cache-dir -r nolcat/requirements.txt
 
 WORKDIR ./nolcat/
-CMD python -m pytest -s --log-cli-level="{log_level}" -p pytest_session2file --session2file=testfile_{datetime.timestamp().localtime()}.txt{test_script_name}
+CMD python -m pytest -s --log-cli-level="{log_level}" -p pytest_session2file --session2file=testfile_{datetime.now().isoformat()}.txt{test_script_name}
 """
 with open('Dockerfile', 'w') as dockerfile:
     dockerfile.write(dockerfile_text)
