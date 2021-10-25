9cda7cbf5dfe7430a8577aa4eb502c6966dd7bd4
Elissa_Reiskind <ereiskind@fsu.edu>
2021-10-25 10:29:49 -0400

    Save logfile timestamp to variable

--- tests/run_nolcat_tests.py
+++ tests/run_nolcat_tests.py
@@ -15,6 +15,7 @@ if test_script_name != "":
 
 #Section: Create Image
 #Subsection: Create New Dockerfile
+logfile_timestamp = datetime.now().strftime('%Y-%m-%dT%H.%M.%S%z')
 dockerfile_text = f"""
 FROM python:3.8
 WORKDIR ./
@@ -24,7 +25,7 @@ RUN git clone https://github.com/ereiskind/nolcat.git -b {branch_name} ./nolcat/
 RUN pip install --no-cache-dir -r nolcat/requirements.txt
 
 WORKDIR ./nolcat/
-CMD python -m pytest -s --log-cli-level="{log_level}" -p pytest_session2file --session2file=logfile_{datetime.now().strftime('%Y-%m-%dT%H.%M.%S%z')}.txt{test_script_name}
+CMD python -m pytest -s --log-cli-level="{log_level}" -p pytest_session2file --session2file=logfile_{logfile_timestamp}.txt{test_script_name}
 """
 with open('Dockerfile', 'w') as dockerfile:
     dockerfile.write(dockerfile_text)
@@ -35,7 +36,7 @@ subprocess.call("docker build -t nolcat-image --no-cache .")
 
 #Section: Create Container, Running Tests and Generating Logs
 subprocess.call("docker run -it --name nolcat-container nolcat-image")
-subprocess.call(f"docker cp nolcat-container:/logfile_{datetime.now().strftime('%Y-%m-%dT%H.%M.%S%z')}.txt ./tests/logs/logfile_{datetime.now().strftime('%Y-%m-%dT%H.%M.%S%z')}.txt")
+subprocess.call(f"docker cp nolcat-container:/logfile_{logfile_timestamp}.txt ./tests/logs/logfile_{logfile_timestamp}.txt")
 
 
 #Section: Remove Docker Objects
