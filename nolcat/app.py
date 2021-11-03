58b391c57a9bd9f434b0ff598900636e99c2b0cb
Elissa_Reiskind <ereiskind@fsu.edu>
2021-11-03 15:46:13 -0400

    Work to resolve merge conflicts

--- nolcat/app.py
+++ nolcat/app.py
@@ -1,14 +1,23 @@
+from pathlib import Path
 from flask import Flask
 from flask import render_template
 from flask_wtf.csrf import CSRFProtect
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from nolcat.ingest import forms  #ToDo: If routes are still in this file when `view` blueprint is added, add `as ingest_forms`
 
 csrf = CSRFProtect()
 
+def page_not_found(error):
+    """Returns the 404 page when a HTTP 404 error is raised."""
+    return render_template('404.html'), 404
+
+
 def create_app():
     """A factory pattern for instantiating Flask web apps."""
     app = Flask(__name__)
+    app.register_error_handler(404, page_not_found)
     csrf.init_app(app)
     #ToDo: Replace regerating secret key with reference to container environment variable
     app.config['SECRET_KEY'] = "ReplaceMeLater"
@@ -16,9 +25,13 @@ def create_app():
     from nolcat import ingest
     app.register_blueprint(ingest.bp)
 
+    #ToDo: from nolcat import view
+    #ToDo: app.register_blueprint(view.bp)
+    #ToDo: Create `view` blueprint
 
     @app.route('/')
     def homepage():
+        """Returns the homepage in response to web app root requests."""
         return render_template('index.html')
     
 
@@ -37,3 +50,24 @@ def create_app():
         return render_template('index.html')
 
     return app
+
+
+def Chrome_browser_driver():
+    """Creates a Selenium webdriver for a headless Chrome browser."""
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--verbose')
+    chrome_options.add_experimental_option("prefs", {
+            "download.default_directory": "Downloads",
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing_for_trusted_sources_enabled": False,
+            "safebrowsing.enabled": False
+    })
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-software-rasterizer')
+
+    return webdriver.Chrome(options=chrome_options)
--- nolcat/ingest/views.py
+++ nolcat/ingest/views.py
@@ -3,15 +3,56 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 from flask import request
+from flask import abort
+import pandas as pd
 from . import bp
 from ingest.forms import *
+from nolcat.raw_COUNTER_report import *
+
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")  # This formatting puts the appearance of these logging messages largely in line with those of the Flask logging messages
 
 
+#ToDo: After creating the first account with ingest permissions, come here
 @bp.route('/initialize-database')
-def start_R4_data_load():
+def initialize_initial_relations():
     """Returns the page where the CSVs with R4 data are selected."""
+    return render_template('start-database-initialization.html')
+
+
+@bp.route('/initialize-collection-tracking')
+def save_historical_collection_tracking_info():
+    #ToDo: Load "initialize_fiscalYears.csv" into titular relation
+    #ToDo: Load "initialize_vendors.csv" into titular relation
+    #ToDo: Load "initialize_statisticsSources.csv" into titular relation
+    #ToDo: `SELECT statisticsSources.Statistics_Source_ID, fiscalYears.Fiscal_Year_ID, statisticsSources.Statistics_Source_Name, fiscalYears.Year FROM statisticsSources JOIN fiscalYears;` (this is an intentional cartesian product)
+    #ToDo: Create downloadable CSV "initialize_annualUsageCollectionTracking.csv" with results of above as first four columns and the following field names in the rest of the first row
+        # Usage_Is_Being_Collected
+        # Manual_Collection_Required
+        # Collection_Via_Email
+        # Is_COUNTER_Compliant
+        # Collection_Status
+        # Usage_File_Path
+        # Notes
+    return render_template('historical-collection-tracking.html')
+
+
+@bp.route('/historical-non-COUNTER-data')
+def upload_historical_non_COUNTER_usage():
+    #ToDo: Load "initialize_annualUsageCollectionTracking.csv" into titular relation
+    #Alert: The procedure below is based on non-COUNTER compliant usage being in files saved in container and retrieved by having their paths saved in the database; if the files themselves are saved in the database as BLOB objects, this will need to change
+    #ToDo: `SELECT AUCT_Statistics_Source, AUCT_Fiscal_Year FROM annualUsageCollectionTracking WHERE Usage_File_Path='true';`
+    #ToDo: Create an iterable to pass all the records returned by the above to a form
+    render_template('upload-historical-non-COUNTER-data.html')
+
+
+@bp.route('/historical-COUNTER-data')
+def upload_historical_COUNTER_usage():
+    """Saves non-COUNTER compliant usage data files and their locations, then returns the page for uploading reformatted COUNTER R4 CSVs."""
+    #Alert: The procedure below is based on non-COUNTER compliant usage being in files saved in container and retrieved by having their paths saved in the database; if the files themselves are saved in the database as BLOB objects, this will need to change
+    #ToDo: For each file uploaded in the form
+        #ToDo: Save the file in a TBD location in the container using the AUCT_Statistics_Source and AUCT_Fiscal_Year values for the file name
+        #ToDo: `UPDATE annualUsageCollectionTracking SET Usage_File_Path='<file path of the file saved above>' WHERE AUCT_Statistics_Source=<the composite PK value> AND AUCT_Fiscal_Year=<the composite PK value>`
     return render_template('select-R4-CSVs.html')
 
 
