f748ae1361b06b1994d229e961a08ba1c663c9aa
Elissa_Reiskind <ereiskind@fsu.edu>
2021-11-29 16:07:26 -0500

    Create test module for SQLAlchemy

---
+++ tests/test_SQLAlchemy.py
@@ -0,0 +1,10 @@
+"""This module contains the tests for SQLAlchemy."""
+
+import pytest
+
+from nolcat.app import engine
+
+
+def test_engine_creation():
+    """Test that a SQLAlchemy engine can be created."""
+    pass
