9a41d52cda1e863cb6f18ef8f72533995fd96291
Elissa_Reiskind <ereiskind@fsu.edu>
2021-11-30 15:13:33 -0500

    Create test_loading_into_relation test

--- tests/test_database_integration.py
+++ tests/test_database_integration.py
@@ -2,6 +2,7 @@
 
 import pytest
 import pandas as pd
+from pandas._testing import assert_frame_equal
 
 from nolcat.app import engine
 
@@ -27,12 +28,30 @@ def resources_relation():
 
 def test_engine_creation():
     """Test that a SQLAlchemy engine can be created."""
+    #ToDo: Is this needed as a separate test, and if so, how should it be done?
     pass
 
 
-def test_loading_into_relation():
+def test_loading_into_relation(engine, resources_relation):
     """Tests loading a single dataframe into a relation."""
-    pass
+    connection = engine.connect()
+    transaction = connection.begin()
+    resources_relation.to_sql(
+        name='resources',
+        con=engine,
+        if_exists='replace',
+        chunksize=1000,
+        index=True,
+        index_label='Resource_ID',
+    )
+
+    retrieved_data = pd.read_sql(
+        sql="SELECT * FROM resources;",
+        con=engine
+    )
+
+    assert_frame_equal(resources_relation, retrieved_data)
+    transaction.rollback()
 
 
 def test_loading_into_database():
