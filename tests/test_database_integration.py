453757c3ad3b0d2cea29278f57ced7c544329447
Elissa_Reiskind <ereiskind@fsu.edu>
2021-11-30 10:30:01 -0500

    Create database integration tests with docstrings

--- tests/test_database_integration.py
+++ tests/test_database_integration.py
@@ -8,3 +8,13 @@ from nolcat.app import engine
 def test_engine_creation():
     """Test that a SQLAlchemy engine can be created."""
     pass
+
+
+def test_loading_into_relation():
+    """Tests loading a single dataframe into a relation."""
+    pass
+
+
+def test_loading_into_database():
+    """Tests breaking apart a dataframe and loading it into the database."""
+    pass 
