2e3566147a7eb16f7a6f1998ffecb2c6066d1967
Elissa Reiskind <ereiskind@fsu.edu>
2021-12-25 10:23:21 -0500

    Create SUSHI credentials fixture

--- tests/test_SUSHICallAndResponse.py
+++ tests/test_SUSHICallAndResponse.py
@@ -1,3 +1,25 @@
 import pytest
 
 from nolcat.ingest.SUSHI_call_and_response import SUSHICallAndResponse
+
+@pytest.fixture
+def SUSHI_credentials_fixture():
+    """A fixture to collect and store the data for making SUSHI calls."""
+    URL = input("Enter the SUSHI base URL, including the final backslash: ")
+    user_id = input("Enter the SUSHI customer ID: ")
+    requestor_id = input("Enter the SUSHI requestor ID or just press enter if none exists: ")
+    api_key = input("Enter the SUSHI API key or just press enter if none exists: ")
+    platform = input("Enter the SUSHI platform or just press enter if none exists: ")
+
+    SUSHI_credentials = dict(user_id = user_id)
+    if requestor_id != "":
+        SUSHI_credentials['requestor_id'] = requestor_id
+    if api_key != "":
+        SUSHI_credentials['api_key'] = api_key
+    if platform != "":
+        SUSHI_credentials['platform'] = platform
+    
+    print(URL)
+    print(SUSHI_credentials)
+
+    yield (URL, SUSHI_credentials)
