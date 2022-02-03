8c188951a378e9e4833cc99d3ddad092db902f80
Elissa_Reiskind <ereiskind@fsu.edu>
2022-02-03 11:34:35 -0500

    Correct `customer_id` parameter name

--- tests/test_SUSHICallAndResponse.py
+++ tests/test_SUSHICallAndResponse.py
@@ -18,12 +18,12 @@ For testing the SUSHI API, a fixture that prompts the user for the SUSHI API inf
 def SUSHI_credentials_fixture():
     """A fixture to collect and store the data for making SUSHI calls."""
     URL = input("Enter the SUSHI base URL, including the final backslash: ")
-    user_id = input("Enter the SUSHI customer ID: ")
+    customer_id = input("Enter the SUSHI customer ID: ")
     requestor_id = input("Enter the SUSHI requestor ID or just press enter if none exists: ")
     api_key = input("Enter the SUSHI API key or just press enter if none exists: ")
     platform = input("Enter the SUSHI platform or just press enter if none exists: ")
 
-    SUSHI_credentials = dict(user_id = user_id)
+    SUSHI_credentials = dict(customer_id = customer_id)
     if requestor_id != "":
         SUSHI_credentials['requestor_id'] = requestor_id
     if api_key != "":
