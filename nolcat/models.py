1634b9b443f5fbada2e13890e8f0cca86ede8d82
Elissa_Reiskind <ereiskind@fsu.edu>
2022-02-11 10:25:14 -0500

    Add data types to StatisticsSresourceSources

--- nolcat/models.py
+++ nolcat/models.py
@@ -423,9 +423,9 @@ class StatisticsResourceSources(Base):
     __tablename__ = 'statisticsResourceSources'
     __table_args__ = {'schema': 'nolcat'}
 
-    srs_statistics_sources = Column(ForeignKey('nolcat.StatisticsSources.statistics_source_id'))  #ToDo: INT NOT NULL
-    srs_resource_sources = Column(ForeignKey('nolcat.ResourceSources.resource_source_id'))  #ToDo: INT NOT NULL
-    current_statistics_source = Column()  #ToDo: BOOLEAN NOT NULL
+    srs_statistics_sources = Column(Integer, ForeignKey('nolcat.StatisticsSources.statistics_source_id'))
+    srs_resource_sources = Column(Integer, ForeignKey('nolcat.ResourceSources.resource_source_id'))
+    current_statistics_source = Column(Boolean)
 
     statisticsSources_FK_statisticsResourceSources = relationship('StatisticsSources', backref='statistics_source_id')
     resourceSources_FK_statisticsResourceSources = relationship('ResourceSources', backref='resource_source_id')
