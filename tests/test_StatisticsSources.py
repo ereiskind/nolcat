f24ddd479eefaa7c042656d415350b6707949fcb
Elissa_Reiskind <ereiskind@fsu.edu>
2022-02-11 09:38:28 -0500

    Write StatisticsSources_fixture

--- tests/test_StatisticsSources.py
+++ tests/test_StatisticsSources.py
@@ -1,5 +1,7 @@
 """Tests the methods in StatisticsSources."""
 
+import json
+from random import randint
 import pytest
 
 from nolcat.models import StatisticsSources
@@ -10,25 +12,22 @@ from nolcat.models import PATH_TO_CREDENTIALS_FILE
 def StatisticsSources_fixture():
     """A fixture mocking up a StatisticsSources object based on the R5 SUSHI credentials JSON."""
     #ToDo: The fixture currently counts the number of StatisticsSources in the R5 SUSHI credentials JSON and currently chooses a number in that range for the fixture's StatisticsSources.statistics_source_retrieval_code value; all the other attributes are placeholders with static values. Should the randomly selected number be used to retrieve actual values for a StatisticsSources object instead?
-    #Section: Get the Number of Statistics Sources in the JSON
-    #ToDo: number_of_StatisticsSources = 0
-    #ToDo: with open(path_to_credentials_file) as JSON_file:
-            #ToDo: SUSHI_data_file = json.load(JSON_File)
-            #ToDo: for vendor in SUSHI_data_file:
-                #ToDo: for stats_source in vendor:
-                    #ToDo: if stats_source['interface_id']:
-                        #ToDo: number_of_StatisticsSources += 1
+    number_of_StatisticsSources = 0
+    with open(PATH_TO_CREDENTIALS_FILE) as JSON_file:
+        SUSHI_data_file = json.load(JSON_file)
+        for vendor in SUSHI_data_file:
+            for stats_source in vendor:
+                if stats_source['interface_id']:
+                    number_of_StatisticsSources += 1
     
-    
-    #Section: Create the StatisticsSources Object
-    #ToDo: random_StatisticsSources_value = str(random number between 1 and number_of_StatisticsSources)
-    #ToDo: fixture_value = StatisticsSources(
-        # 1,  # StatisticsSources.statistics_source_id
-        # "StatisticsSources.statistics_source_name",
-        # random_StatisticsSources_value,
-        # 1,  # StatisticsSources.vendor_id
-    # )
-    #ToDo: return fixture_value
+    random_StatisticsSources_value = str(randint(1, number_of_StatisticsSources))
+    fixture_value = StatisticsSources(
+        1,  # StatisticsSources.statistics_source_id
+        "StatisticsSources.statistics_source_name",
+        random_StatisticsSources_value,
+        1,  # StatisticsSources.vendor_id
+    )
+    return fixture_value
 
 
 def test_fetch_SUSHI_credentials_for_API(StatisticsSources_fixture):
