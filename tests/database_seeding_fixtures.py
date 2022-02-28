fb00fad818f2ddfdc72d4a3ded3ef894afcb140a
Elissa_Reiskind <ereiskind@fsu.edu>
2022-02-28 10:20:40 -0500

    Create database_seeding_fixtures module

---
+++ tests/database_seeding_fixtures.py
@@ -0,0 +1,41 @@
+"""These fixtures add content to any blank relations in the NoLCAT database so all tables have data that can be used in running tests."""
+
+import pytest
+
+#ToDo: Create fixture for fiscalYears
+
+
+#ToDo: Create fixture for vendors
+
+
+#ToDo: Create fixture for vendorNotes
+
+
+#ToDo: Create fixture for statisticsSources
+
+
+#toDo: Create fixture for statisticsSourceNotes
+
+
+#ToDo: Create fixture for statisticsResourceSources
+
+
+#ToDo: Create fixture for resourceSources
+
+
+#ToDo: Create fixture for resourceSourceNotes
+
+
+#ToDo: Create fixture for annualUsageCollectionTracking
+
+
+#ToDo: Create fixture for resources
+
+
+#ToDo: Create fixture for resourceTitles
+
+
+#ToDo: Create fixture for resourcePlatforms
+
+
+#ToDo: Create fixture for usageData
