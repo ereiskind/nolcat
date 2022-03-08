7dc0872fb3b8c9806d554d542e098fee5dae21ac
Elissa_Reiskind <ereiskind@fsu.edu>
2022-03-08 11:35:30 -0500

    Rename engine fixture for disambiguation

--- tests/test_StatisticsSources.py
+++ tests/test_StatisticsSources.py
@@ -13,7 +13,7 @@ from database_seeding_fixtures import statisticsSources_relation
 
 
 @pytest.fixture(scope="module")
-def engine():
+def engine_fixture():
     """Creates a SQLAlchemy engine object by calling the `create_engine` function with the appropriate variables."""
     yield engine()
 
@@ -24,9 +24,9 @@ def engine():
 #ToDo: Create fixture to load into the statisticsSources relation based on the import but replacing the values in statisticsSources_relation['Statistics_Source_Retrieval_Code'] with random retrieval code values found in R5_SUSHI_credentials.json
 
 
-def test_engine_creation(engine):
+def test_engine_creation(engine_fixture):
     """Test that a SQLAlchemy engine is created."""
-    assert repr(type(engine)) == "<class 'sqlalchemy.engine.base.Engine'>"
+    assert repr(type(engine_fixture)) == "<class 'sqlalchemy.engine.base.Engine'>"
 
 
 #ToDo: Load the imported vendors_relation dataframe and the above modified statisticsSources_relation dataframe into the database
