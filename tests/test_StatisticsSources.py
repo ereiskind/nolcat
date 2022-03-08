7ebce3df3f01a584b8defe5b19789c3b5b4f8717
Elissa_Reiskind <ereiskind@fsu.edu>
2022-03-08 10:03:28 -0500

    Frame preliminaries in StatisticsSources test module

--- tests/test_StatisticsSources.py
+++ tests/test_StatisticsSources.py
@@ -1,5 +1,24 @@
 """Tests the methods in StatisticsSources."""
 
 import pytest
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from nolcat import Database_Credentials
 from nolcat.models import StatisticsSources
+from nolcat.models import PATH_TO_CREDENTIALS_FILE
+from database_seeding_fixtures import vendors_relation
+from database_seeding_fixtures import statisticsSources_relation
+
+
+#ToDo: Create SQLAlchemy engine fixture
+
+
+#ToDo: Create SQLAlchemy session fixture
+
+
+#ToDo: Create fixture to load into the statisticsSources relation based on the import but replacing the values in statisticsSources_relation['Statistics_Source_Retrieval_Code'] with random retrieval code values found in R5_SUSHI_credentials.json
+
+
+#ToDo: Load the imported vendors_relation dataframe and the above modified statisticsSources_relation dataframe into the database
