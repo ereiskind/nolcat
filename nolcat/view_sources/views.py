46977a2f112b7e13656356fc3f1d8abda15bd56b
Elissa Reiskind <ereiskind@fsu.edu>
2022-04-20 12:46:27 -0400

    Add to-do regarding relationships

--- nolcat/view_sources/views.py
+++ nolcat/view_sources/views.py
@@ -23,3 +23,4 @@ logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")  # T
     # Edit resourceSources details
     #ToDo: Adding sources uses blank fields where editing resources prepopulates them and saves any changes--are the source types similar enough that they can use the same template?
     #ToDo: This includes adding to notes relations for sources
+    #ToDo: Changing a statisticsSources-resourceSources connection means changing the non-PK field in statisticsResourceSources from true to false and creating a new record with the PKs of the new sources--does it makes sense to have a "if stats source changes, pick new one here" drop-down listing all stats sources but the current one on a resource source details page?
