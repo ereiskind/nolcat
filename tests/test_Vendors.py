246253e49d546067e77b8f1c55a4d3fd03f3da34
Elissa_Reiskind <ereiskind@fsu.edu>
2022-04-21 12:35:57 -0400

    Create `Vendors` test module

---
+++ tests/test_Vendors.py
@@ -0,0 +1,23 @@
+"""Tests the methods in Vendors."""
+
+import pytest
+
+from nolcat.models import Vendors
+
+
+def test_get_SUSHI_credentials_from_Alma():
+    """Create a test for the function."""
+    #ToDo: ???
+    pass
+
+
+def test_get_SUSHI_credentials_from_JSON():
+    """Create a test for the function."""
+    #ToDo: ???
+    pass
+
+
+def test_add_note():
+    """Create a test for the function."""
+    #ToDo: ???
+    pass
