3b73c13153835d5d6202645c1b2164afe9d4e56c
Elissa_Reiskind <ereiskind@fsu.edu>
2022-06-27 16:40:51 -0400

    Create `StatisticsSourceNotes` docstring

--- nolcat/models.py
+++ nolcat/models.py
@@ -396,7 +396,15 @@ class StatisticsSources(db.Model):
 
 
 class StatisticsSourceNotes(db.Model):
-    """A relation containing notes about statistics sources."""
+    """The class representation of the `statisticsSourceNotes` relation, which contains notes about the statistics sources in `statisticsSources`.
+    
+    Attributes:
+        self.statistics_source_notes_ID (int): the primary key
+        self.note (text): the content of the note
+        self.written_by (str): the note author
+        self.date_written (date): the day the note was last edited
+        self.statistics_source_ID (int): the foreign key for `statisticsSources`
+    """
     __tablename__ = 'statisticsSourceNotes'
 
     statistics_source_notes_ID = db.Column(db.Integer, primary_key=True)
