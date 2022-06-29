3161d25667c9702874eff94bbada657473370889
Elissa_Reiskind <ereiskind@fsu.edu>
2022-06-29 09:55:48 -0400

    Remove test stdout from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,8 +9,6 @@ from nolcat.app import create_app
 def app():
     """Creates an instance of the Flask web app for testing."""
     app = create_app()
-    print(f"Created app {app} of type {repr(type(app))}")
     app.testing = True  # Lets exceptions come through to test client
     with app.test_client() as client:
-        print(f"Created client {client} of type {repr(type(client))}")
         yield client
