86fb3fe913cbad78093661aa37a767e19064a373
Elissa_Reiskind <ereiskind@fsu.edu>
2022-07-18 11:57:37 -0400

    Finish writing test_DR_call_validity

--- tests/test_SUSHICallAndResponse.py
+++ tests/test_SUSHICallAndResponse.py
@@ -133,10 +133,10 @@ def test_DR_call_validity(SUSHI_credentials_fixture):
     URL, SUSHI_credentials = SUSHI_credentials_fixture
     check_for_report = SUSHICallAndResponse("StatisticsSources.statistics_source_name", URL, "reports", SUSHI_credentials).make_SUSHI_call()
     has_DR = False
-    #ToDo: list_of_reports = check_for_report.values()
-    #ToDo: for report in list_of_reports:
-        #ToDo: if report["Report_ID"] == "DR":  # Know this key will be found because if it couldn't be, ``test_reports_call_validity`` wouldn't have passed
-            #toDo: has_DR = True
+    list_of_reports = check_for_report.values()
+    for report in list_of_reports:
+        if report["Report_ID"] == "DR":  # Know this key will be found because if it couldn't be, ``test_reports_call_validity`` wouldn't have passed
+            has_DR = True
     if has_DR == False:
         pytest.skip("DR not offered by this vendor.")
     response = SUSHICallAndResponse("StatisticsSources.statistics_source_name", URL, "reports/dr", SUSHI_credentials).make_SUSHI_call()
