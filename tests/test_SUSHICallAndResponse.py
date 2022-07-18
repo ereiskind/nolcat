2dcfd06ad2495b74d14d5c12d6d01249100cc68b
Elissa_Reiskind <ereiskind@fsu.edu>
2022-07-18 11:56:57 -0400

    Finish writing test_PR_call_validity

--- tests/test_SUSHICallAndResponse.py
+++ tests/test_SUSHICallAndResponse.py
@@ -117,10 +117,10 @@ def test_PR_call_validity(SUSHI_credentials_fixture):
     URL, SUSHI_credentials = SUSHI_credentials_fixture
     check_for_report = SUSHICallAndResponse("StatisticsSources.statistics_source_name", URL, "reports", SUSHI_credentials).make_SUSHI_call()
     has_PR = False
-    #ToDo: list_of_reports = check_for_report.values()
-    #ToDo: for report in list_of_reports:
-        #ToDo: if report["Report_ID"] == "PR":  # Know this key will be found because if it couldn't be, ``test_reports_call_validity`` wouldn't have passed
-            #toDo: has_PR = True
+    list_of_reports = check_for_report.values()
+    for report in list_of_reports:
+        if report["Report_ID"] == "PR":  # Know this key will be found because if it couldn't be, ``test_reports_call_validity`` wouldn't have passed
+            has_PR = True
     if has_PR == False:
         pytest.skip("PR not offered by this vendor.")
     response = SUSHICallAndResponse("StatisticsSources.statistics_source_name", URL, "reports/pr", SUSHI_credentials).make_SUSHI_call()
