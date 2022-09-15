1da1af011a0fb4d78fd8b5da0e201b8496a07595
Elissa Reiskind <ereiskind@fsu.edu>
2022-09-15 10:40:44 -0400

    Add menu for choosing relation to output to TSV

--- tests/create_relation_TSV.py
+++ tests/create_relation_TSV.py
@@ -1,1 +1,26 @@
 """This module outputs the test data used in the relations as TSVs."""
+
+import pyinputplus as pyip
+
+fixture = pyip.inputMenu(
+    prompt="Enter the number of the relation that should be output to a TSV.\n",
+    choices=[
+        "fiscalYears",
+        "vendors",
+        #ToDo: vendorNotes,
+        "statisticsSources",
+        #ToDo: statisticsSourceNotes,
+        "statisticsResourceSources",
+        "resourceSources",
+        #ToDo: resourceSourceNotes,
+        "annualUsageCollectionTracking",
+        "resources",
+        "resourceMetadata",
+        "resourcePlatforms",
+        "usageData",
+    ],
+    numbered=True,
+)
+
+print(type(fixture))
+print(fixture)
