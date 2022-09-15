b479676b8cc59889be135ffbef12e533361c23e8
Elissa Reiskind <ereiskind@fsu.edu>
2022-09-15 10:50:33 -0400

    Add imports of dataframe creation functions

--- tests/create_relation_TSV.py
+++ tests/create_relation_TSV.py
@@ -2,6 +2,8 @@
 
 import pyinputplus as pyip
 
+from tests.data import relations
+
 fixture = pyip.inputMenu(
     prompt="Enter the number of the relation that should be output to a TSV.\n",
     choices=[
@@ -21,6 +23,36 @@ fixture = pyip.inputMenu(
     ],
     numbered=True,
 )
+
+if fixture == "fiscalYears":
+    fixture = relations.fiscalYears_relation()
+elif fixture == "vendors":
+    fixture = relations.vendors_relation()
+elif fixture == "vendorNotes":
+    #fixture = relations.vendorNotes_relation()
+    pass  #ToDo: Update when dataframe is created 
+elif fixture == "statisticsSources":
+    fixture = relations.statisticsSources_relation()
+elif fixture == "statisticsSourceNotes":
+    #fixture = relations.statisticsSourceNotes_relation()
+    pass  #ToDo: Update when dataframe is created
+elif fixture == "statisticsResourceSources":
+    fixture = relations.statisticsResourceSources_relation()
+elif fixture == "resourceSources":
+    fixture = relations.resourceSources_relation()
+elif fixture == "resourceSourceNotes":
+    #fixture = relations.resourceSourceNotes_relation()
+    pass  #ToDo: Update when dataframe is created
+elif fixture == "annualUsageCollectionTracking":
+    fixture = relations.annualUsageCollectionTracking_relation()
+elif fixture == "resources":
+    fixture = relations.resources_relation()
+elif fixture == "resourceMetadata":
+    fixture = relations.resourceMetadata_relation()
+elif fixture == "resourcePlatforms":
+    fixture = relations.resourcePlatforms_relation()
+elif fixture == "usageData":
+    fixture = relations.usageData_relation()
 
 print(type(fixture))
 print(fixture)
