08075c5a9e5641e91f03a061591d3a5844e65130
ereiskind <65416187+ereiskind@users.noreply.github.com>
2022-09-21 15:29:03 -0400

    Correct typo

--- tests/test_bp_annual_stats.py
+++ tests/test_bp_annual_stats.py
@@ -7,7 +7,7 @@ from nolcat.app import create_app
 from nolcat.annual_stats import *
 
 
-#ToDo: Create test for route to view homepagepage, which is static
+#ToDo: Create test for route to view homepage, which is static
 
 
 #ToDo: Create test for route to page with details of a fiscal year (display of data in a given fiscalYears record)
