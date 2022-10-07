103aef0efe0ed71aa9dfba2cc9314d8e75100ec1
ereiskind <65416187+ereiskind@users.noreply.github.com>
2022-10-07 12:27:27 -0400

    Update `app.py` based off `main`

--- nolcat/app.py
+++ nolcat/app.py
@@ -1,13 +1,24 @@
 from pathlib import Path
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-from nolcat.ingest import forms  #ToDo: If routes are still in this file when `view` blueprint is added, add `as ingest_forms`
+"""Since GitHub is used to manage the code, and the repo is public, secret information is stored in a file exclusive to the container and imported into this file.
+The overall structure of this app doesn't facilitate a separate module for a SQLAlchemy `create_engine` function: when `nolcat/__init__.py` is present, keeping these functions in a separate module and importing them causes a ``ModuleNotFoundError: No module named 'database_connectors'`` error when starting up the Flask server, but with no init file, the blueprint folder imports don't work. With Flask-SQLAlchemy, a string for the config variable `SQLALCHEMY_DATABASE_URI` is all that's needed, so the data the string needs are imported from the `secrets.py` file here.
+"""
+from .. import nolcat_secrets as secrets
+
+DATABASE_USERNAME = secrets.Username
+DATABASE_PASSWORD = secrets.Password
+DATABASE_HOST = secrets.Host
+DATABASE_PORT = secrets.Port
+DATABASE_SCHEMA_NAME = secrets.Database
+SECRET_KEY = secrets.Secret
+
 
 csrf = CSRFProtect()
+db = SQLAlchemy()
 
 def page_not_found(error):
     """Returns the 404 page when a HTTP 404 error is raised."""
@@ -19,26 +30,65 @@ def create_app():
     app = Flask(__name__)
     app.register_error_handler(404, page_not_found)
     csrf.init_app(app)
-    #ToDo: Replace regerating secret key with reference to container environment variable
-    app.config['SECRET_KEY'] = "ReplaceMeLater"
+    db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_SCHEMA_NAME}'
+    app.config['SECRET_KEY'] = SECRET_KEY
     app.config['UPLOAD_FOLDER'] = './data'
 
-    from nolcat import ingest
-    app.register_blueprint(ingest.bp)
+    #Section: Create Command to Build Schema
+    # Documentation at https://flask.palletsprojects.com/en/2.1.x/appcontext/
+    @app.cli.command('create-db')
+    def create_db():
+        with create_app().app_context():
+            from .models import FiscalYears
+            from .models import Vendors
+            from .models import VendorNotes
+            from .models import StatisticsSources
+            from .models import StatisticsSourceNotes
+            from .models import StatisticsResourceSources
+            from .models import ResourceSources
+            from .models import ResourceSourceNotes
+            from .models import AnnualUsageCollectionTracking
+            from .models import Resources
+            from .models import ResourceMetadata
+            from .models import ResourcePlatforms
+            from .models import UsageData
+            db.create_all()
 
-    from nolcat import view
-    app.register_blueprint(view.bp)
+    #Section: Create Homepage and Register Other Blueprints
+    from ..nolcat import annual_stats
+    app.register_blueprint(annual_stats.bp)
+
+    from ..nolcat import ingest_usage
+    app.register_blueprint(ingest_usage.bp)
+
+    from ..nolcat import initialization
+    app.register_blueprint(initialization.bp)
+
+    from ..nolcat import login
+    app.register_blueprint(login.bp)
+
+    from ..nolcat import view_resources
+    app.register_blueprint(view_resources.bp)
+
+    from ..nolcat import view_sources
+    app.register_blueprint(view_sources.bp)
+
+    from ..nolcat import view_usage
+    app.register_blueprint(view_usage.bp)
+
+    from ..nolcat import view_vendors
+    app.register_blueprint(view_vendors.bp)
 
     @app.route('/')
     def homepage():
         """Returns the homepage in response to web app root requests."""
-        #ToDo: Add login for `ingest` blueprint
-        #ToDo: Add login for `view` blueprint
         return render_template('index.html')
     
 
     #Section: Routes Involving Forms
     #ToDo: Figure out how to manage CSRF tokens with a nested source code folder
+    """ These are sample form creation and submission routes 
     @app.route('/enter-data')
     def enter_data():
         form = forms.TestForm()
@@ -50,30 +100,6 @@ def create_app():
         if form.validate_on_submit():
             return render_template('ok.html', val=form.string.data)
         return render_template('index.html')
+    """
 
     return app
-
-
-def Chrome_browser_driver():
-    """Creates a Selenium webdriver for a headless Chrome browser."""
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--verbose')
-    chrome_options.add_experimental_option("prefs", {
-            "download.default_directory": "Downloads",
-            "download.prompt_for_download": False,
-            "download.directory_upgrade": True,
-            "safebrowsing_for_trusted_sources_enabled": False,
-            "safebrowsing.enabled": False
-    })
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-software-rasterizer')
-
-    ''' From the first program
-    Path_to_ChromeDriver = Path('..', 'usr', 'local', 'bin', 'chromedriver.exe') # On Windows, "chromedriver" must include the ".exe" extension; on Linux, when the file extension is included, there's a WebDriverException with the message "'chromedriver.exe' executable needs to be in PATH."
-    return webdriver.Chrome(options=chrome_options, executable_path=Path_to_ChromeDriver)
-    '''
-    return webdriver.Chrome(options=chrome_options)
