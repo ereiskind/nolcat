a63975060eff9cd8a100bfa3c774734f43a590ff
ereiskind <65416187+ereiskind@users.noreply.github.com>
2022-11-17 10:59:35 -0500

    Update docstrings for UploadCOUNTERReports

--- nolcat/upload_COUNTER_reports.py
+++ nolcat/upload_COUNTER_reports.py
@@ -1,29 +1,29 @@
 class UploadCOUNTERReports:
     """A class for transforming uploaded Excel workbook(s) with COUNTER data into dataframes ready for normalization.
 
-    This class exists to transform the tabular COUNTER reports in the Excel workbooks in the `ImmutableMultiDict` objects created by file uploads into a single dataframe ready for normalization, most frequently in the form of initialization as a `RawCOUNTERReport` object. As the description above reveals, the desired behavior is more that of a function than a class; that would-be function becomes a class by dividing it into the traditional `__init__` method, which instantiates the `ImmutableMultiDict` as a class attribute, and the `create_dataframe()` method, which performs the actual transformation. This structure requires all instances of the class constructor to be prepended to a call to the `create_dataframe()` method, which means objects of the `UploadCOUNTERReports` type are never instantiated.
+    COUNTER reports not delivered by SUSHI are given in a tabular format and usually saved in Excel workbooks. These workbooks can be ingested into this program via a Flask-WTF MultipleFileField form field, but that workbook data requires a great deal of manipulation and cleaning to become a single dataframe ready for normalization, most frequently in the form of initialization as a `RawCOUNTERReport` object. This class exists to make those changes; since the desired behavior is more that of a function than a class, the would-be function becomes a class by dividing it into the traditional `__init__` method, which instantiates the MultipleFileField object encapsulating the selected Excel workbook(s) as a class attribute, and the `create_dataframe()` method, which performs the actual transformation. This structure requires all instances of the class constructor to be prepended to a call to the `create_dataframe()` method, which means objects of the `UploadCOUNTERReports` type are never instantiated.
 
     Attributes:
-        self.COUNTER_report_files (ImmutableMultiDict): the uploaded Excel workbook(s) of tabular COUNTER data
+        self.COUNTER_report_files (MultipleFileField): The constructor method for `UploadCOUNTERReports`, which instantiates the MultipleFileField object.
 
     Methods:
-        create_dataframe: This method transforms tabular COUNTER reports in uploaded Excel workbooks into a single dataframe ready for normalization.
+        create_dataframe: This method transforms the data from the tabular COUNTER reports in uploaded Excel workbooks into a single dataframe ready for normalization.
     """
     def __init__(self, COUNTER_report_files):
-        """The constructor method for `UploadCOUNTERReports`, which sets the attribute values for each instance.
+        """The constructor method for `UploadCOUNTERReports`, which instantiates the MultipleFileField object.
 
-        This constructor is not meant to be used alone; all class instantiations should feature a `create_dataframe()` method call.
+        This constructor is not meant to be used alone; all class instantiations should have a `create_dataframe()` method call appended to it.
 
         Args:
-            COUNTER_report_files (ImmutableMultiDict): the uploaded Excel workbook(s) of tabular COUNTER data
+            COUNTER_report_files (MultipleFileField): The MultipleFileField object containing the uploaded Excel workbook(s) of tabular COUNTER data
         """
         self.COUNTER_report_files = COUNTER_report_files
     
 
     def create_dataframe(self):
-        """This method transforms tabular COUNTER reports in uploaded Excel workbooks into a single dataframe ready for normalization.
+        """This method transforms the data from the tabular COUNTER reports in uploaded Excel workbooks into a single dataframe ready for normalization.
 
-        This method transforms tabular COUNTER reports on sheets in Excel workbooks into dataframes, but to become complete and valid records in the relation, data not found in the tabular report is also required. The statistics source of the data will be derived from the Excel file name, which follows the naming convention `<Statistics_Source_ID>_<calendar year assigned to fiscal year in "yyyy" format>.xlsx` (the year is used for disambiguation and file name uniqueness, not data). For each individual report, the type of report is taken from the sheet name, which is the official report type abbreviation.
+        This method prepares the data from tabular COUNTER reports for upload into the database. This method transforms tabular COUNTER reports on sheets in Excel workbooks into dataframes, but to become complete and valid records in the relation, enhancements are needed, including cleaning the data, filling in data R4 provided through its multiple different report types, and adding the statistics source of the data, which is taken from the first part of the file name.
 
         Returns:
             dataframe: COUNTER data ready for normalization
