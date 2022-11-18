8e786336f9b2800ef32a61fd1d6be4eae09d4fb6
ereiskind <65416187+ereiskind@users.noreply.github.com>
2022-11-18 12:18:06 -0500

    Get standardized field names

--- nolcat/upload_COUNTER_reports.py
+++ nolcat/upload_COUNTER_reports.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import datetime
 from flask import request
 from openpyxl import load_workbook
 
@@ -77,4 +78,102 @@ class UploadCOUNTERReports:
                         break
                     else:
                         header_row_number += 1
+                
+
+                #Section: Get the Field Names
+                df_field_names = []
+                df_date_field_names = []
+                for iterable_of_field_names in sheet.iter_rows(
+                    min_row=header_row_number,
+                    max_row=header_row_number,
+                    min_col=1,
+                    max_col=number_of_fields,
+                    values_only=True,
+                ):  # Creates a tuple with the field names as elements
+                    for field_name in iterable_of_field_names:
+                        logging.debug(f"Getting standardized field name for field {field_name} (type {type(field_name)})")
+                        try:
+                            date_as_string = re.findall(r'([A-Z][a-z]{2})\-(\d{4})', string=field_name)  # `None` in `findall` raises an error, so it needs to be in a `try-except` block
+                        except:
+                            date_as_string = False
+                        
+                        if field_name == "ISSN" and (report_type == 'BR1' or report_type == 'BR2' or report_type == 'BR3' or report_type == 'BR5'):
+                            df_field_names.append("Online_ISSN")  # This is the first name replacement because assigning a certain type of ISSN changes the meaning slightly
+                        
+                        elif date_as_string:
+                            date_tuple = date_as_string[0]
+                            if date_tuple[0] == "Jan":
+                                month_int = 1
+                            elif date_tuple[0] == "Feb":
+                                month_int = 2
+                            elif date_tuple[0] == "Mar":
+                                month_int = 3
+                            elif date_tuple[0] == "Apr":
+                                month_int = 4
+                            elif date_tuple[0] == "May":
+                                month_int = 5
+                            elif date_tuple[0] == "Jun":
+                                month_int = 6
+                            elif date_tuple[0] == "Jul":
+                                month_int = 7
+                            elif date_tuple[0] == "Aug":
+                                month_int = 8
+                            elif date_tuple[0] == "Sep":
+                                month_int = 9
+                            elif date_tuple[0] == "Oct":
+                                month_int = 10
+                            elif date_tuple[0] == "Nov":
+                                month_int = 11
+                            elif date_tuple[0] == "Dec":
+                                month_int = 12
+                            df_field_names.append(datetime.date(int(date_tuple[1]), month_int, 1))
+                            df_date_field_names.append(datetime.date(int(date_tuple[1]), month_int, 1))
+                        elif repr(type(field_name)) == "<class 'datetime.datetime'>":
+                            df_field_names.append(datetime.date(field_name.year, field_name.month, 1))  # This both ensures the date is the first of the month and removes the unneeded time data
+                            df_date_field_names.append(datetime.date(field_name.year, field_name.month, 1))
+                        
+                        elif field_name is None and (report_type == 'BR1' or report_type == 'BR2' or report_type == 'BR3' or report_type == 'BR5'):
+                            df_field_names.append("Resource_Name")
+                        elif field_name == "Collection" and report_type == 'MR1':
+                            df_field_names.append("Resource_Name")
+                        elif field_name == "Database" and (report_type == 'DB1' or report_type == 'DB2'):
+                            df_field_names.append("Resource_Name")
+                        
+                        elif field_name == "Journal" and (report_type == 'JR1' or report_type == 'JR2'):
+                            df_field_names.append("Resource_Name")
+                        elif field_name == "Title" and (report_type == 'BR1' or report_type == 'BR2' or report_type == 'BR3' or report_type == 'BR5' or report_type == 'TR1' or report_type == 'TR2'):
+                            df_field_names.append("Resource_Name")
+                        elif field_name == "Database" and report_type == 'DR':
+                            df_field_names.append("Resource_Name")
+                        elif field_name == "Title" and report_type == 'TR':
+                            df_field_names.append("Resource_Name")
+                        elif field_name == "Item" and report_type == 'IR':
+                            df_field_names.append("Resource_Name")
+                        
+                        elif field_name == "Content Provider" and report_type == 'MR1':
+                            df_field_names.append("Publisher")
+                        elif field_name == "User Activity" and (report_type == 'BR5' or report_type == 'DB1'or report_type == 'PR1'):
+                            df_field_names.append("Metric_Type")
+                        elif (field_name == "Access Denied Category" or field_name == "Access denied category") and (report_type == 'BR3' or report_type == 'DB2' or report_type == 'JR2' or report_type == 'TR2'):
+                            df_field_names.append("Metric_Type")
+                        
+                        # These changes are for bringing R4 reports in line with R5 naming conventions, so they don't need to be limited to certain reports
+                        elif field_name == "Proprietary Identifier":
+                            df_field_names.append("Proprietary_ID")
+                        elif field_name == "Book DOI" or field_name == "Journal DOI" or field_name == "Title DOI":
+                            df_field_names.append("DOI")
+                        elif field_name == "Data type" or field_name == "Data Type":
+                            df_field_names.append("Data_Type")
+                        elif field_name == "Online ISSN":
+                            df_field_names.append("Online_ISSN")
+                        elif field_name == "Print ISSN":
+                            df_field_names.append("Print_ISSN")
+
+                        elif field_name is None:
+                            continue  # Deleted data and merged cells for header values can make Excel think null columns are in use; when read, these columns add `None` to `df_field_names`, causing a `ValueError: Number of passed names did not patch number of header fields in the file` when reading the worksheet contents into a dataframe
+                        
+                        else:
+                            df_field_names.append(field_name)
+                df_non_date_field_names = [field_name for field_name in df_field_names if field_name not in df_date_field_names]  # List comprehension used to preserve order
+                logging.info(f"The COUNTER report contains the fields {df_non_date_field_names} and data for the dates {df_date_field_names}.")
         pass
