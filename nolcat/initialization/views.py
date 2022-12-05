19c5fb4a3d8a6f051cf5e963383c30146e6cdb36
ereiskind <65416187+ereiskind@users.noreply.github.com>
2022-12-05 15:50:40 -0500

    Replace `infer_objects` with `astype` methods

--- nolcat/initialization/views.py
+++ nolcat/initialization/views.py
@@ -67,7 +67,11 @@ def collect_initial_relation_data():
             encoding='utf-8',
             encoding_errors='backslashreplace',
         )
-        fiscalYears_dataframe = fiscalYears_dataframe.infer_objects()
+        fiscalYears_dataframe = fiscalYears_dataframe.astype({
+            'fiscal_year': pd.StringDtype,
+            'notes_on_statisticsSources_used': pd.StringDtype,
+            'notes_on_corrections_after_submission': pd.StringDtype,
+        })
         logging.info(f"`fiscalYears` dataframe dtypes before encoding conversions:\n{fiscalYears_dataframe.dtypes}\n")  # When the statement below raises an error, this logging statement doesn't appear
         #fiscalYears_dataframe['notes_on_statisticsSources_used'] = fiscalYears_dataframe['notes_on_statisticsSources_used'].apply(lambda value: value if value.isnull() == True else value.encode('utf-8').decode('unicode-escape'))  #ToDo: Determine how this expression raised `AttributeError: 'float' object has no attribute 'isnull'` when all the values in the referenced series are blank--why does this series seem to have a float data type?
         #fiscalYears_dataframe['notes_on_corrections_after_submission'] = fiscalYears_dataframe['notes_on_corrections_after_submission'].apply(lambda value: value if value.isnull() == True else value.encode('utf-8').decode('unicode-escape'))
@@ -86,7 +90,10 @@ def collect_initial_relation_data():
             encoding='utf-8',
             encoding_errors='backslashreplace',
         )
-        vendors_dataframe = vendors_dataframe.infer_objects()
+        vendors_dataframe = vendors_dataframe.astype({
+            'vendor_name': pd.StringDtype,
+            'alma_vendor_code': pd.StringDtype,
+        })
         logging.info(f"`vendors` dataframe dtypes before encoding conversions:\n{vendors_dataframe.dtypes}\n")
         #vendors_dataframe['vendor_name'] = vendors_dataframe['vendor_name'].apply(lambda value: value if value.isnull() == True else value.encode('utf-8').decode('unicode-escape'))
         logging.info(f"`vendors` dataframe:\n{vendors_dataframe}\n")
@@ -107,7 +114,10 @@ def collect_initial_relation_data():
             encoding='utf-8',
             encoding_errors='backslashreplace',
         )
-        vendorNotes_dataframe = vendorNotes_dataframe.infer_objects()
+        vendorNotes_dataframe = vendorNotes_dataframe.astype({
+            'note': pd.StringDtype,
+            'written_by': pd.StringDtype,
+        })
         logging.info(f"`vendorNotes` dataframe dtypes before encoding conversions:\n{vendorNotes_dataframe.dtypes}\n")
         #vendorNotes_dataframe['note'] = vendorNotes_dataframe['note'].apply(lambda value: value if value.isnull() == True else value.encode('utf-8').decode('unicode-escape'))
         logging.info(f"`vendorNotes` dataframe:\n{vendorNotes_dataframe}\n")
@@ -126,7 +136,10 @@ def collect_initial_relation_data():
             encoding='utf-8',
             encoding_errors='backslashreplace',
         )
-        statisticsSources_dataframe = statisticsSources_dataframe.infer_objects()
+        statisticsSources_dataframe = statisticsSources_dataframe.astype({
+            'statistics_source_name': pd.StringDtype,
+            'statistics_source_retrieval_code': pd.StringDtype,
+        })
         logging.info(f"`statisticsSources` dataframe dtypes before encoding conversions:\n{statisticsSources_dataframe.dtypes}\n")
         #statisticsSources_dataframe['statistics_source_name'] = statisticsSources_dataframe['statistics_source_name'].apply(lambda value: value if value.isnull() == True else value.encode('utf-8').decode('unicode-escape'))
         logging.info(f"`statisticsSources` dataframe:\n{statisticsSources_dataframe}\n")
@@ -147,7 +160,10 @@ def collect_initial_relation_data():
             infer_datetime_format=True,
             encoding_errors='backslashreplace',
         )
-        statisticsSourceNotes_dataframe = statisticsSourceNotes_dataframe.infer_objects()
+        statisticsSourceNotes_dataframe = statisticsSourceNotes_dataframe.astype({
+            'note': pd.StringDtype,
+            'written_by': pd.StringDtype,
+        })
         logging.info(f"`statisticsSourceNotes` dataframe dtypes before encoding conversions:\n{statisticsSourceNotes_dataframe.dtypes}\n")  # When the statement below raises an error, this logging statement doesn't appear
         #statisticsSourceNotes_dataframe['note'] = statisticsSourceNotes_dataframe['note'].apply(lambda value: value if value.isnull() == True else value.encode('utf-8').decode('unicode-escape'))
         logging.info(f"`statisticsSourceNotes` dataframe:\n{statisticsSourceNotes_dataframe}\n")
@@ -169,7 +185,9 @@ def collect_initial_relation_data():
             encoding='utf-8',
             encoding_errors='backslashreplace',
         )
-        resourceSources_dataframe = resourceSources_dataframe.infer_objects()
+        resourceSources_dataframe = resourceSources_dataframe.astype({
+            'resource_source_name': pd.StringDtype,
+        })
         logging.info(f"`resourceSources` dataframe dtypes before encoding conversions:\n{resourceSources_dataframe.dtypes}\n")
         #resourceSources_dataframe['resource_source_name'] = resourceSources_dataframe['resource_source_name'].apply(lambda value: value if value.isnull() == True else value.encode('utf-8').decode('unicode-escape'))
         logging.info(f"`resourceSources` dataframe:\n{resourceSources_dataframe}\n")
@@ -190,7 +208,10 @@ def collect_initial_relation_data():
             encoding='utf-8',
             encoding_errors='backslashreplace',
         )
-        resourceSourceNotes_dataframe = resourceSourceNotes_dataframe.infer_objects()
+        resourceSourceNotes_dataframe = resourceSourceNotes_dataframe.astype({
+            'note': pd.StringDtype,
+            'written_by': pd.StringDtype,
+        })
         logging.info(f"`resourceSourceNotes` dataframe dtypes before encoding conversions:\n{resourceSourceNotes_dataframe.dtypes}\n")  # When the statement below raises an error, this logging statement doesn't appear
         #resourceSourceNotes_dataframe['note'] = resourceSourceNotes_dataframe['note'].apply(lambda value: value if value.isnull() == True else value.encode('utf-8').decode('unicode-escape'))
         logging.info(f"`resourceSourceNotes` dataframe:\n{resourceSourceNotes_dataframe}\n")
@@ -208,7 +229,6 @@ def collect_initial_relation_data():
             encoding='utf-8',
             encoding_errors='backslashreplace',
         )
-        statisticsResourceSources_dataframe = statisticsResourceSources_dataframe.infer_objects()
         # Because there aren't any string dtypes in need of encoding correction, the logging statements for the dtypes and the dataframe have been combined
         logging.info(f"`statisticsResourceSources` dtypes and dataframe:\n{statisticsResourceSources_dataframe.dtypes}\n{statisticsResourceSources_dataframe}\n")
 
