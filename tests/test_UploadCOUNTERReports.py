a874c6d81ee3eff585ec6b6c9cf7c62d4546f241
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-01-17 13:15:35 -0500

    Add comparison of dataframes to logging stdout

--- tests/test_UploadCOUNTERReports.py
+++ tests/test_UploadCOUNTERReports.py
@@ -49,5 +49,5 @@ def sample_COUNTER_reports():
 def test_create_dataframe(sample_COUNTER_report_workbooks, sample_COUNTER_reports):
     """Tests transforming multiple Excel workbooks with tabular COUNTER data into a single dataframe ready for the RawCOUNTERReport class."""
     df = UploadCOUNTERReports(sample_COUNTER_report_workbooks).create_dataframe()
-    print(df)
+    print(df.compare(sample_COUNTER_reports))
     assert assert_frame_equal(df, sample_COUNTER_reports)
