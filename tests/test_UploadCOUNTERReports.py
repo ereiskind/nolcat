0191d2065c32fdcd5670f9e9fa32281e8478f4f0
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-01-17 15:50:03 -0500

    Add dataframe comparison to logging stdout

--- tests/test_UploadCOUNTERReports.py
+++ tests/test_UploadCOUNTERReports.py
@@ -49,7 +49,5 @@ def sample_COUNTER_reports():
 def test_create_dataframe(sample_COUNTER_report_workbooks, sample_COUNTER_reports):
     """Tests transforming multiple Excel workbooks with tabular COUNTER data into a single dataframe ready for the RawCOUNTERReport class."""
     df = UploadCOUNTERReports(sample_COUNTER_report_workbooks).create_dataframe()
-    print(f"`df`:\n{df.columns}")
-    print(f"`sample_counter_reports`:\n{sample_COUNTER_reports.columns}")
-    #print(df.compare(sample_COUNTER_reports))
+    print(df.compare(sample_COUNTER_reports))
     assert assert_frame_equal(df, sample_COUNTER_reports, check_like=True)  # Keyword argument allows test to pass if fields aren't in the same order
