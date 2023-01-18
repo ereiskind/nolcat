6ea5f2f87abc92f003d6b6e22e026f0385d4eb18
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-01-18 17:43:35 -0500

    Change reference to field to use `loc`

--- nolcat/upload_COUNTER_reports.py
+++ nolcat/upload_COUNTER_reports.py
@@ -487,7 +487,7 @@ class UploadCOUNTERReports:
         combined_df = combined_df.astype(combined_df_dtypes, errors='ignore')  #ToDo: Will ignoring data type conversion errors cause problems with loading into MySQL?
         if "publication_date" in combined_df_field_names:
             combined_df['publication_date'] = pd.to_datetime(
-                combined_df['publication_date'],  # Or `combined_df.loc[combined_df.columns.tolist().index("publication_date")]` per a comment on https://stackoverflow.com/a/49756813
+                combined_df.loc[combined_df.columns.tolist().index("publication_date")],  # Per a comment on https://stackoverflow.com/a/49756813
                 errors='coerce',  # Changes the null values to the date dtype's null value `NaT`
                 infer_datetime_format=True,
             )
