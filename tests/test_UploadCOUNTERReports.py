1ca51fe911ee0b9277e9c14d85bd1ffff2d9bdc0
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-01-19 16:53:35 -0500

    Check alignment of field names in logging stdout

--- tests/test_UploadCOUNTERReports.py
+++ tests/test_UploadCOUNTERReports.py
@@ -49,8 +49,7 @@ def sample_COUNTER_reports():
 def test_create_dataframe(sample_COUNTER_report_workbooks, sample_COUNTER_reports):
     """Tests transforming multiple Excel workbooks with tabular COUNTER data into a single dataframe ready for the RawCOUNTERReport class."""
     df = UploadCOUNTERReports(sample_COUNTER_report_workbooks).create_dataframe()
-    print(f"`df` fields:\n{df.columns.tolist()}")
-    print(f"`sample_COUNTER_reports` fields:\n{sample_COUNTER_reports.columns.tolist()}")
     field_order = df.columns.tolist()
+    print(f"The fields are set up the same: {df[field_order].columns.tolist() == sample_COUNTER_reports[field_order].columns.tolist()}")
     print(df[field_order].set_index('resource_name').compare(sample_COUNTER_reports[field_order].set_index('resource_name')))
     assert assert_frame_equal(df, sample_COUNTER_reports, check_like=True)  # Keyword argument allows test to pass if fields aren't in the same order
