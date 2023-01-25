8a49b128dbc3265d47d6d7f444c0b2ec0ab18d88
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-01-25 12:54:10 -0500

    Finish framing `annual_stats_homepage` route function

--- nolcat/annual_stats/views.py
+++ nolcat/annual_stats/views.py
@@ -1,5 +1,9 @@
 import logging
 from flask import render_template
+from flask import request
+from flask import abort
+from flask import redirect
+from flask import url_for
 
 from . import bp
 from ..app import db
@@ -10,10 +14,23 @@ from .forms import ChooseFiscalYearForm
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")  # This formatting puts the appearance of these logging messages largely in line with those of the Flask logging messages
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def annual_stats_homepage():
     """Returns the homepage for the `annual_stats` blueprint, which serves as a homepage for administrative functions."""
-    return render_template('annual_stats/index.html')
+    form = ChooseFiscalYearForm()
+    if request.method == 'GET':
+        # The links to the lists of vendors, resource sources, and statistics sources are standard jinja redirects; the code for populating the lists from the underlying relations is in the route functions being redirected to
+        fiscal_year_options = pd.read_sql(
+            sql="SELECT fiscal_year_ID, fiscal_year FROM fiscalYears;",
+            con=db.engine,
+        )
+        form.fiscal_year.choices = list(fiscal_year_options['fiscal_year_ID', 'fiscal_year'].itertuples(index=False, name=None))
+        return render_template('annual_stats/index.html', form=form)
+    elif form.validate_on_submit():
+        fiscal_year_PK = form.fiscal_year.data
+        return redirect(url_for('show_fiscal_year_details'))  #ToDo: Use https://stackoverflow.com/a/26957478 to add variable path information
+    else:
+        return abort(404)
 
 
 #ToDo: Create route for details of a fiscal year
