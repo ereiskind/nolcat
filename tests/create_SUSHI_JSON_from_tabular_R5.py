3e69311c53fe2685d4480f669786472517cfaa25
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-01-30 09:44:09 -0500

    Update paths and remove testing outputs

--- tests/create_SUSHI_JSON_from_tabular_R5.py
+++ tests/create_SUSHI_JSON_from_tabular_R5.py
@@ -1,11 +1,12 @@
-from pathlib import Path
+import pathlib
 import re
 from datetime import date
 import json
 from openpyxl import load_workbook
 import pandas as pd
 
-save_file_path = Path('C:\\Users\ereiskind\OneDrive - Florida State University\Documents')
+absolute_path_to_tests_directory = pathlib.Path(__file__).parent.resolve()
+directory_with_final_JSONs = absolute_path_to_tests_directory / 'data' / 'COUNTER_JSONs_for_tests'
 
 # All methods for importing `app.py` from `nolcat` failed when running the module in both the VS Code debugger and Command Prompt, so the function is repeated here
 def last_day_of_month(first_day_of_month):
@@ -25,7 +26,7 @@ def last_day_of_month(first_day_of_month):
 
 #Section: Load the Workbook(s)
 file_path = input("Enter the complete file path for the Excel workbook output from OpenRefine: ")
-file_path = Path(file_path)
+file_path = pathlib.Path(file_path)
 file = load_workbook(filename=file_path, read_only=True)
 report_name = file_path.stem
 report_type = report_name.split("_")[1]
@@ -103,50 +104,32 @@ join_index_fields = grouping_fields + ['Begin_Date']
 instance_grouping_fields = grouping_fields + ['Begin_Date']
 #ALERT: Below raised `ValueError: cannot insert Begin_Date, already exists` for DR but not PR
 instance_df = (df.groupby(instance_grouping_fields)).apply(lambda instance: instance[['Metric_Type', 'Count']].to_dict('records')).reset_index().rename(columns={0: "Instance"})  # `grouping_fields` can be used for the groupby fields because it includes the two fields included in the instance grouping plus a field that doesn't need to be in the dataframe
-#instance_df.to_csv(save_file_path/'1.csv')
-#instance_df.to_json(save_file_path/'1.json', force_ascii=False, indent=4, orient='table', index=False)
 instance_df = instance_df.set_index(join_index_fields)
-#instance_df.to_csv(save_file_path/'2.csv')
-#instance_df.to_json(save_file_path/'2.json', force_ascii=False, indent=4, orient='table', index=False)
 
 #Subsection: Create Period Grouping
 df['temp'] = range(1, len(df.index)+1)  # The way groupby works, for each resource defined by metadata, if a given instance (a metric and its count) occurs in multiple months, those months will be combined, which is not the desired behavior; this field puts a unique value in each row/record, which prevents this grouping
 period_grouping_fields = grouping_fields + ['Metric_Type', 'Count', 'temp']
 period_df = df.groupby(period_grouping_fields).apply(lambda period: period[['Begin_Date','End_Date']].to_dict('records')).reset_index().rename(columns={0: "Period"})
-#period_df.to_csv(save_file_path/'3.csv')
-#period_df.to_json(save_file_path/'3.json', force_ascii=False, indent=4, orient='table', index=False)
 period_df = period_df.drop(columns=['temp', 'Metric_Type', 'Count'])
 period_df['Period'] = period_df['Period'].map(lambda list_like: list_like[0])
 period_df['Begin_Date'] = period_df['Period'].astype('string').map(lambda string: string[16:-28])  # This turns the JSON/dict value into a string, then isolates the desired date from within each string
-#period_df.to_csv(save_file_path/'4.csv')
-#period_df.to_json(save_file_path/'4.json', force_ascii=False, indent=4, orient='table', index=False)
 period_df = period_df.set_index(join_index_fields)
-#period_df.to_csv(save_file_path/'5.csv')
-#period_df.to_json(save_file_path/'5.json', force_ascii=False, indent=4, orient='table', index=False)
 
 #Subsection: Combine Period and Instance Groupings
 combined_df = period_df.join(instance_df, how='inner')  # This contains duplicate records
-#combined_df.to_csv(save_file_path/'6.csv')
-#combined_df.to_json(save_file_path/'6.json', force_ascii=False, indent=4, orient='table', index=False)
 combined_df = combined_df.reset_index()
 combined_df = combined_df.drop(columns=['Begin_Date'])
 combined_df['instance_string'] = combined_df['Instance'].astype('string')
 combined_df['period_string'] = combined_df['Period'].astype('string')
-#combined_df.to_csv(save_file_path/'7.csv')
-#combined_df.to_json(save_file_path/'7.json', force_ascii=False, indent=4, orient='table', index=False)
 
 #Subsection: Create Final JSON
 combined_df['repeat'] = combined_df.duplicated(subset=['Platform', 'Data_Type', 'Access_Method', 'instance_string', 'period_string'], keep='first')
 combined_df = combined_df.loc[combined_df['repeat'] == False]  # Where the Boolean indicates if the record is the same as an earlier record
 combined_df = combined_df.drop(columns=['instance_string', 'period_string', 'repeat'])
 combined_df = combined_df.reset_index(drop=True)
-#combined_df.to_csv(save_file_path/'8.csv')
-#combined_df.to_json(save_file_path/'8.json', force_ascii=False, indent=4, orient='table', index=False)
 final_df = combined_df.groupby(grouping_fields).apply(lambda performance: performance[['Period', 'Instance']].to_dict('records')).reset_index().rename(columns={0: "Performance"})
-#final_df.to_csv(save_file_path/'9.csv')
-#final_df.to_json(save_file_path/'9.json', force_ascii=False, indent=4, orient='table', index=False)
 
 
 #Section: Output JSON
-final_JSON_output_path = Path('C:\\Users', 'ereiskind', 'nolcat', 'tests', 'data', 'COUNTER_JSONs_for_tests', 'result_JSON.json')
+final_JSON_output_path = pathlib.Path('data', 'COUNTER_JSONs_for_tests', 'result_JSON.json')
 final_df.to_json(final_JSON_output_path, force_ascii=False, indent=4, orient='table', index=False)
