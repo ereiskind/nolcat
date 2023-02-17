46c6982b6955941b1115abe62358dc891dd90712
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-02-17 08:55:20 -0500

    Revise print statements and correct indentation

--- tests/test_data_load.py
+++ tests/test_data_load.py
@@ -34,8 +34,7 @@ def load_test_data_into_database(engine):
         sql=f"SELECT COUNT(*) FROM {relation_name};",
         con=engine,
     )
-    print(f"`check_for_data`: {check_for_data}")
-    print(f"`type(check_for_data)`: {type(check_for_data)}")
+    print(f"Number of records currently in relation: {check_for_data.iloc[0]['COUNT(*)']}")
     if not check_for_data.iloc[0]['COUNT(*)'] == 0:  # Query above is requesting the number of records in the relation, so an empty relation returns the integer `0`
         return f"The `{relation_name}` relation already has data in it, so there will either be an error when attempting to load the test data or data in other relations won't be appropriately matched. To prevent either of those problems, the program is quitting now without loading any data into the selected relation; please use the MySQL command line or the `db-actions` script's truncate function, both in the instance, to remove all data from the relation before trying again."  #ToDo: Shorten string to prevent removal of middle section in pytest display
     else:
@@ -43,7 +42,7 @@ def load_test_data_into_database(engine):
             sql=f"SELECT * FROM {relation_name};",
             con=engine,
         )
-        print(f"`show_data`:\n{show_data}")
+        print(f"Records to be loaded into relation:\n{show_data}")
     
     if relation_name == "fiscalYears":
         relation_data = relations.fiscalYears_relation()
@@ -76,7 +75,7 @@ def load_test_data_into_database(engine):
     except exc.IntegrityError as error:
         return f"The `to_sql` method raised an IntegrityError: {error.orig.args}"
     except Exception as error:
-            return f"The `to_sql` method raised an exception: {error.orig.args}"
+        return f"The `to_sql` method raised an exception: {error.orig.args}"
 
     return f"The test data was loaded into {relation_name}."
 
