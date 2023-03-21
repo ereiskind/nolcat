113a52b7b4fb5b4378f0ff219755d1a7f0e0571b
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-03-21 10:17:28 -0400

    Add logging

--- tests/create_SUSHI_JSON_from_tabular_R5.py
+++ tests/create_SUSHI_JSON_from_tabular_R5.py
@@ -1,6 +1,9 @@
 """Transform Excel worksheet produced by "tests\\data\\create_JSON_base.json" into a JSON."""
 
+import logging
 import pathlib
+
+logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s] %(message)s")
 
 absolute_path_to_tests_directory = pathlib.Path(__file__).parent.resolve()
 directory_with_final_JSONs = absolute_path_to_tests_directory / 'data' / 'COUNTER_JSONs_for_tests'
