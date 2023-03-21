20466eb8f1f656f8aa8a44715eb2ec308b70ea1d
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-03-21 10:15:39 -0400

    Add file path constants

--- tests/create_SUSHI_JSON_from_tabular_R5.py
+++ tests/create_SUSHI_JSON_from_tabular_R5.py
@@ -1,1 +1,6 @@
 """Transform Excel worksheet produced by "tests\\data\\create_JSON_base.json" into a JSON."""
+
+import pathlib
+
+absolute_path_to_tests_directory = pathlib.Path(__file__).parent.resolve()
+directory_with_final_JSONs = absolute_path_to_tests_directory / 'data' / 'COUNTER_JSONs_for_tests'
