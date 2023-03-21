bb859c8d28b52e69ff365170c099e530fef19e3f
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-03-21 17:21:23 -0400

    Rename fixture and update docstring

--- tests/test_StatisticsSources.py
+++ tests/test_StatisticsSources.py
@@ -16,8 +16,15 @@ from nolcat.models import PATH_TO_CREDENTIALS_FILE
 
 #Section: Fixtures
 @pytest.fixture(scope='session')
-def most_recent_month_with_usage():
-    """Creates the value that will be used for the `begin_date` SUSHI parameter and for database queries in other locations in the testing module."""
+def first_day_of_most_recent_month_with_usage():
+    """Creates the value that will be used for the `begin_date` SUSHI parameter.
+
+    Some StatisticsSources methods call other parts of the code that make SUSHI calls, but since that testing isn't the focus of this module, values unlikely to raise errors are supplied to be passed to the SUSHI API calls. In the case of the dates, the most recent month for which usage is likely to be available is used as both the start and end of the available date range, as more recent data is less likely to be in the database and thus cause a problem with the check for previously loaded data.
+
+    Yields:
+        datetime.date: the first day of a month
+    """
+    
     current_date = datetime.date.today()
     if current_date.day < 10:
         begin_month = current_date + relativedelta(months=-2)
@@ -28,14 +35,14 @@ def most_recent_month_with_usage():
 
 
 @pytest.fixture(scope='session')
-def StatisticsSources_fixture(engine, most_recent_month_with_usage):
+def StatisticsSources_fixture(engine, first_day_of_most_recent_month_with_usage):
     """A fixture simulating a `StatisticsSources` object containing the necessary data to make a real SUSHI call.
     
     The SUSHI API has no test values, so testing SUSHI calls requires using actual SUSHI credentials. This fixture creates a `StatisticsSources` object with mocked values in all fields except `statisticsSources_relation['Statistics_Source_Retrieval_Code']`, which uses a random value taken from the R5 SUSHI credentials file. Because the `_harvest_R5_SUSHI()` method includes a check preventing SUSHI calls to stats source/date combos already in the database, stats sources current with the available usage statistics are filtered out to prevent their use.
 
     Args:
         PATH_TO_CREDENTIALS_FILE (str): the file path for "R5_SUSHI_credentials.json"
-        most_recent_month_with_usage (datetime.date): the most recent month for which COUNTER data is available
+        first_day_of_most_recent_month_with_usage (datetime.date): the first day of the most recent month for which COUNTER data is available
 
     Yields:
         StatisticsSources: a StatisticsSources object connected to valid SUSHI data
@@ -52,7 +59,7 @@ def StatisticsSources_fixture(engine, most_recent_month_with_usage):
     retrieval_codes = []
     for interface in retrieval_codes_as_interface_IDs:
         query_result = pd.read_sql(
-            sql=f"SELECT COUNT(*) FROM statisticsSources JOIN COUNTERData ON statisticsSources.statistics_source_ID=COUNTERData.statistics_source_ID WHERE statisticsSources.statistics_source_retrieval_code={interface} AND COUNTERData.usage_date={most_recent_month_with_usage.strftime('%Y-%m-%d')}",
+            sql=f"SELECT COUNT(*) FROM statisticsSources JOIN COUNTERData ON statisticsSources.statistics_source_ID=COUNTERData.statistics_source_ID WHERE statisticsSources.statistics_source_retrieval_code={interface} AND COUNTERData.usage_date={first_day_of_most_recent_month_with_usage.strftime('%Y-%m-%d')}",
             con=engine,
         )
         if not query_result.empty or not query_result.isnull().all().all():  # `empty` returns Boolean based on if the dataframe contains data elements; `isnull().all().all()` returns a Boolean based on a dataframe of Booleans based on if the value of the data element is null or not
@@ -82,7 +89,7 @@ def test_fetch_SUSHI_information_for_display(StatisticsSources_fixture):
     pass
 
 
-def test_harvest_R5_SUSHI(StatisticsSources_fixture, most_recent_month_with_usage):
+def test_harvest_R5_SUSHI(StatisticsSources_fixture, first_day_of_most_recent_month_with_usage):
     """Tests collecting all available R5 reports for a `StatisticsSources.statistics_source_retrieval_code` value and combining them into a single dataframe."""
     begin_test = datetime.datetime.now()
     end_date = datetime.date(
@@ -91,7 +98,7 @@ def test_harvest_R5_SUSHI(StatisticsSources_fixture, most_recent_month_with_usag
         calendar.monthrange(most_recent_month_with_usage.year, most_recent_month_with_usage.month)[1],
     )
     before_data_collection = datetime.datetime.now()
-    SUSHI_data = StatisticsSources_fixture._harvest_R5_SUSHI(most_recent_month_with_usage, end_date)
+    SUSHI_data = StatisticsSources_fixture._harvest_R5_SUSHI(first_day_of_most_recent_month_with_usage, end_date)
     data_collected = datetime.datetime.now()
     print(SUSHI_data)
     print(f"The test function start is at {begin_test}, the data collection start is at {before_data_collection}, and the data collection end is at {data_collected}; can any of these be compared to the timestamp in the report to further confirm accuracy?")
@@ -99,16 +106,16 @@ def test_harvest_R5_SUSHI(StatisticsSources_fixture, most_recent_month_with_usag
     assert repr(type(SUSHI_data)) == "<class 'pandas.core.frame.DataFrame'>" and SUSHI_data['statistics_source_ID'].eq(1).all()  #ToDo: and time collected value is equal to one of the above if possible
 
 
-def test_collect_usage_statistics(StatisticsSources_fixture, most_recent_month_with_usage):
+def test_collect_usage_statistics(StatisticsSources_fixture, first_day_of_most_recent_month_with_usage):
     """Tests that the `StatisticsSources.collect_usage_statistics()` successfully loads COUNTER data into the `COUNTERData` relation."""
     end_date = datetime.date(
         most_recent_month_with_usage.year,
         most_recent_month_with_usage.month,
         calendar.monthrange(most_recent_month_with_usage.year, most_recent_month_with_usage.month)[1],
     )
-    #ToDo: to_check_against = StatisticsSources_fixture._harvest_R5_SUSHI(most_recent_month_with_usage, end_date)
+    #ToDo: to_check_against = StatisticsSources_fixture._harvest_R5_SUSHI(first_day_of_most_recent_month_with_usage, end_date)
     #ToDo: number_of_records = to_check_against.shape[0]
-    #ToDo: StatisticsSources_fixture.collect_usage_statistics(most_recent_month_with_usage, end_date)
+    #ToDo: StatisticsSources_fixture.collect_usage_statistics(first_day_of_most_recent_month_with_usage, end_date)
     #ToDo: SQL_query = f"""
     #ToDo:     SELECT *
     #ToDo:     FROM (
