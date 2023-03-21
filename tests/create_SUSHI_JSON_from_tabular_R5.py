5856c14a95ee420fbe4fb426e007cb25bbb0e064
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-03-21 10:25:06 -0400

    Add section to get list of field names

--- tests/create_SUSHI_JSON_from_tabular_R5.py
+++ tests/create_SUSHI_JSON_from_tabular_R5.py
@@ -2,6 +2,7 @@
 
 import logging
 import pathlib
+import re
 from openpyxl import load_workbook
 
 logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s] %(message)s")
@@ -18,3 +19,25 @@ report_name = file_path.stem
 report_type = report_name.split("_")[1]
 sheet = file[report_name]
 logging.info(f"Creating JSON from report {report_name} for a {report_type}.")
+
+
+#Section: Get the Field Names
+# Excel workbooks created by Apache POI programs like OpenRefine don't have their `max_column` and `max_row` attributes set properly, so those values must be reset, after which a loop through rows that breaks after the first row can be used
+sheet.reset_dimensions()
+df_field_names = []
+for row in sheet.rows:
+    for field_name in row:
+        
+        # `None` in regex methods raises a TypeError, so they need to be in try-except blocks
+        try:
+            if re.match(r'^[Cc]omponent', field_name.value):
+                continue  # The rarely used `Component` subtype fields aren't captured by this program
+        except TypeError:
+            pass
+        
+        if field_name.value is None:
+            continue  # Deleted data and merged cells for header values can make Excel think null columns are in use; when read, these columns add `None` to `df_field_names`, causing a `ValueError: Number of passed names did not patch number of header fields in the file` when reading the worksheet contents into a dataframe
+        else:
+            df_field_names.append(field_name.value)
+    break
+logging.info(f"The field names are {df_field_names}.")
