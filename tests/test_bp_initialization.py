c185bc6a3e9f95bd5eb103fc7203ed17c760a00e
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-04-04 10:16:31 -0400

    Double check type and attributes of `POST_request`

--- tests/test_bp_initialization.py
+++ tests/test_bp_initialization.py
@@ -169,10 +169,11 @@ def test_collect_initial_relation_data(tmp_path, header_value, client):
         'resourceSourceNotes_CSV': tmp_path / 'resourceSourceNotes_relation.csv',
         'statisticsResourceSources_CSV': tmp_path / 'statisticsResourceSources_relation.csv',
     })  #ToDo: Is a try-except block that retries with a 299 timeout needed?
-    print(f"`POST_request.encoding`: {POST_request.encoding}")
-    print(f"`POST_request.raise_for_status()`: {POST_request.raise_for_status()}")
-    print(f"`POST_request.status_code`: {POST_request.status_code}")
-    print(f"`POST_request.text`: {POST_request.text}")
+    print(f"`POST_request` (type {type(POST_request)}): {POST_request}\n{dir(POST_request)}")
+    #print(f"`POST_request.encoding`: {POST_request.encoding}")
+    #print(f"`POST_request.raise_for_status()`: {POST_request.raise_for_status()}")
+    #print(f"`POST_request.status_code`: {POST_request.status_code}")
+    #print(f"`POST_request.text`: {POST_request.text}")
     #ToDo: At or after function return statement/redirect, query database for `fiscalYears`, `vendors`, `vendorNotes`, `statisticsSources`, `statisticsSourceNotes`, `resourceSources`, `resourceSourceNotes`, and `statisticsResourceSources` relations and ensure results match files used for submitting data and/or `conftest.py`
     assert True
 
