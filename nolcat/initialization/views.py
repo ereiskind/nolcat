bdd29ceb01ac1ac0e1c42ae26a1a8ba567541af6
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-04-11 13:23:31 -0400

    Remove `render_template()` from return statement

--- nolcat/initialization/views.py
+++ nolcat/initialization/views.py
@@ -250,10 +250,11 @@ def collect_initial_relation_data():
             logging.warning(f"The `to_sql` methods raised an error: {format(error)}")
         
         # `return redirect(url_for('initialization.collect_AUCT_and_historical_COUNTER_data'))` has an initial `POST /initialization/ HTTP/1.1` request and a HTTP 302 response with the redirect URL `http://localhost/initialization/initialization-page-2` and a subsequent GET request to that URL, but the request isn't fully processed by nginx--there's no nginx logging statement, and the browser network tools show a response error `net::ERR_CONNECTION_REFUSED` and no response status code
+        # `return render_template(url_for('initialization.collect_AUCT_and_historical_COUNTER_data') + '.html')` returns an error, the formatted version of which is the string `/initialization/initialization-page-2.html`
         try:
-            return render_template(url_for('initialization.collect_AUCT_and_historical_COUNTER_data') + '.html')
+            return url_for('initialization.collect_AUCT_and_historical_COUNTER_data') + '.html'
         except Exception as error:
-            logging.warning(f"The return statement `render_template(url_for('initialization.collect_AUCT_and_historical_COUNTER_data') + '.html')` returned the error `{format(error)}` (type {type(format(error))})")
+            logging.warning(f"The return statement `url_for('initialization.collect_AUCT_and_historical_COUNTER_data') + '.html'` returned the error `{format(error)}` (type {type(format(error))})")
 
     else:
         return abort(404)
