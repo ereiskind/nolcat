bb4977e67de3d095ecb8aa0032d68bf52de88109
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-04-12 09:30:01 -0400

    Revert return value to correct note

--- nolcat/initialization/views.py
+++ nolcat/initialization/views.py
@@ -255,7 +255,7 @@ def collect_initial_relation_data():
         # `return redirect('/initialization-page-2')` has an initial `POST /initialization/ HTTP/1.1` request and a HTTP 302 response with the redirect URL `http://localhost/initialization/initialization-page-2` and a subsequent GET request to that URL, but the request isn't fully processed by nginx--there's no nginx logging statement, and the browser network tools show a response error `net::ERR_CONNECTION_REFUSED` and no response status code
         # `redirect('collect_AUCT_and_historical_COUNTER_data')` has an initial `POST /initialization/ HTTP/1.1` request and a HTTP 302 response in the nginx log, which also shows a redirect URL of `http://52.91.160.4/initialization/`, but the subsequent GET method isn't fully processed--there's no nginx logging statement, and the browser network tools show a request URL `http://localhost/initialization/collect_AUCT_and_historical_COUNTER_data`, a response error `net::ERR_CONNECTION_REFUSED`, and no response status code
         try:
-            return redirect('collect_AUCT_and_historical_COUNTER_data')
+            return redirect('/initialization-page-2')
         except Exception as error:
             logging.warning(f"The return statement `redirect('collect_AUCT_and_historical_COUNTER_data')` returned the error `{format(error)}` (type {type(format(error))})")
 
