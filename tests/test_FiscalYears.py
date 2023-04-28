6295d3cd5cf6ddf1fda0f25e4290436b708f19cc
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-04-28 15:37:19 -0400

    Correct index field names in test function

--- tests/test_FiscalYears.py
+++ tests/test_FiscalYears.py
@@ -333,7 +333,7 @@ def test_create_usage_tracking_records_for_fiscal_year(engine, client):
     retrieved_data = pd.read_sql(
         sql="SELECT * FROM annualUsageCollectionTracking;",
         con=engine,
-        index_col=['SRS_statistics_source', 'SRS_resource_source'],
+        index_col=["AUCT_statistics_source", "AUCT_fiscal_year"],
     )
     print(f"Info on `retrieved_data` dataframe;\n{return_string_of_dataframe_info(retrieved_data)}")
     multiindex = pd.MultiIndex.from_tuples(
@@ -341,7 +341,7 @@ def test_create_usage_tracking_records_for_fiscal_year(engine, client):
             (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (6, 0), (6, 1), (6, 2), (7, 0), (7, 1), (7, 2), (8, 3), (8, 4), (8, 5), (9, 0), (9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (10, 0), (10, 1), (10, 2), (10, 3), (10, 4), (10, 5), (11, 0), (11, 1), (11, 2), (11, 3), (11, 4), (11, 5),  # Multiindex before method
             (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (9, 6), (10, 6), (11, 6),  # New values in multiindex
         ],
-        names=["SRS_statistics_source", "SRS_resource_source"],
+        names=["AUCT_statistics_source", "AUCT_fiscal_year"],
     )
     expected_output_data = pd.DataFrame(
         [
