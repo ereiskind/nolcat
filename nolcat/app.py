baeada8baa17dad30b28d88bafc35b9d184803e2
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-06-09 17:06:34 -0400

    Set logger default level to `debug`

--- nolcat/app.py
+++ nolcat/app.py
@@ -129,11 +129,10 @@ def create_app():
 def configure_logging(app):
     """Create single logging configuration for entire program. (Based on tutorial at https://shzhangji.com/blog/2022/08/10/configure-logging-for-flask-sqlalchemy-project/.)"""
     logging.basicConfig(format="[%(asctime)s] %(name)s - %(message)s")
-    logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger().setLevel(logging.DEBUG)  # Logging output is limited with the `log-cli-level` argument at the command line
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.DEBUG)
     if app.debug:
-        logging.getLogger().setLevel(logging.DEBUG)
         logging.getLogger('werkzeug').handlers = []  # Prevents Werkzeug from outputting messages twice in debug mode
-        logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 
 def date_parser(dates):
