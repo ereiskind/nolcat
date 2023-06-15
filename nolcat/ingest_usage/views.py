107628ac47f9e7f712c84181eee2fde373826aa2
ereiskind <65416187+ereiskind@users.noreply.github.com>
2023-06-15 11:29:10 -0400

    Get `form.COUNTER_reports` value and type

--- nolcat/ingest_usage/views.py
+++ nolcat/ingest_usage/views.py
@@ -33,6 +33,7 @@ def upload_COUNTER_reports():
         return render_template('ingest_usage/upload-COUNTER-reports.html', form=form)
     elif form.validate_on_submit():
         try:
+            log.info(f"`form.COUNTER_reports` is {form.COUNTER_reports} (type {repr(type(form.COUNTER_reports))})\n{form.COUNTER_reports.__dict__}")
             df = UploadCOUNTERReports(form.COUNTER_reports.data).create_dataframe()  # `form.COUNTER_reports.data` is a list of <class 'werkzeug.datastructures.FileStorage'> objects
             df['report_creation_date'] = pd.to_datetime(None)
             df.index += first_new_PK_value('COUNTERData')
