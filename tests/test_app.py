0c47b1166fec5082e9556cc819ae2a6113bd3c98
ereiskind <ereiskind@fsu.edu>
2023-07-10 17:44:06 -0400

    Correct `test_SQLAlchemy_engine_creation()`

--- tests/test_app.py
+++ tests/test_app.py
@@ -59,7 +59,7 @@ def test_SQLAlchemy_engine_creation(engine):
     """Tests that the fixture for creating the SQLAlchemy engine returned an engine object for connecting to the NoLCAT database."""
     assert isinstance(engine, sqlalchemy.engine.base.Engine)
     assert isinstance(engine.__dict__['url'], sqlalchemy.engine.url.URL)
-    assert str(engine.__dict__['url']) == f'mysql://{DATABASE_USERNAME}:***@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_SCHEMA_NAME}'  # The password is replaced by three asterisks in the `__dict__` output for security
+    assert str(engine.__dict__['url']) == f'mysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_SCHEMA_NAME}'
 
 
 def test_homepage(client):
