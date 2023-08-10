4d1416cfcdc54dd16ec64e08c470d876b7b0cb51
ereiskind <ereiskind@fsu.edu>
2023-08-10 14:29:11 -0400

    Remove reference to uninitialized variable

--- nolcat/initialization/views.py
+++ nolcat/initialization/views.py
@@ -329,9 +329,8 @@ def collect_AUCT_and_historical_COUNTER_data():
 
     #Section: After Form Submission
     elif form.validate_on_submit():
-        log.info(f"Variables initialized in `request.method == 'GET'` exist in `form.validate_on_submit()`: {'df' in locals()}")
-        if template_save_location.is_file():
-            template_save_location.unlink()  #TEST: 2023-08-04 - UnboundLocalError: local variable 'template_save_location' referenced before assignment
+        if 'template_save_location' in locals():  # Submitting the form calls the function again, so the initialized variable isn't saved
+            template_save_location.unlink(missing_ok=True)  #TEST: 2023-08-04 - UnboundLocalError: local variable 'template_save_location' referenced before assignment
         #Subsection: Ingest `annualUsageCollectionTracking` Data
         log.debug(f"`annualUsageCollectionTracking` data:\n{form.annualUsageCollectionTracking_CSV.data}\n")
         AUCT_dataframe = pd.read_csv(
