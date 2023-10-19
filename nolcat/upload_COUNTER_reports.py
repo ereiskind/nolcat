10626b100cb1fcbe4dbed5ccd0a30be34e4ee920
ereiskind <ereiskind@fsu.edu>
2023-10-19 11:53:11 -0400

    Add logging statements for error check

--- nolcat/upload_COUNTER_reports.py
+++ nolcat/upload_COUNTER_reports.py
@@ -279,9 +279,13 @@ class UploadCOUNTERReports:
                 if re.fullmatch(r'PR1?', report_type) is None:
                     log.debug("About to remove total rows from non-platform reports.")  #AboutTo
                     number_of_rows_with_totals = df.shape[1]
+                    log.debug(f"`number_of_rows_with_totals` is {number_of_rows_with_totals} (type {type(number_of_rows_with_totals)})")
                     common_summary_rows = df['resource_name'].str.contains(r'^[Tt]otal\s[Ff]or\s[Aa]ll\s\w*', regex=True)  # `\w*` is because values besides `title` are used in various reports
+                    log.debug(f"`common_summary_rows` is {common_summary_rows} (type {type(common_summary_rows)})")
                     uncommon_summary_rows = df['resource_name'].str.contains(r'^[Tt]otal\s[Ss]earches', regex=True)
+                    log.debug(f"`uncommon_summary_rows` is {uncommon_summary_rows} (type {type(uncommon_summary_rows)})")
                     summary_rows_are_false = ~(common_summary_rows + uncommon_summary_rows)
+                    log.debug(f"`summary_rows_are_false` is {summary_rows_are_false} (type {type(summary_rows_are_false)})")
                     df = df[summary_rows_are_false]
                     log.debug(f"Number of rows in report of type {report_type} reduced from {number_of_rows_with_totals} to {df.shape[1]}.")
 
