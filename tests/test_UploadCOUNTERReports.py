19234f3fb940f1b3c44fa26f5df8e9f853812d46
ereiskind <ereiskind@fsu.edu>
2023-11-03 17:59:09 -0400

    Update `test_create_dataframe()`

--- tests/test_UploadCOUNTERReports.py
+++ tests/test_UploadCOUNTERReports.py
@@ -35,6 +35,7 @@ def sample_COUNTER_report_workbooks():
 #Section: Test
 def test_create_dataframe(sample_COUNTER_report_workbooks, COUNTERData_relation):
     """Tests transforming multiple Excel workbooks with tabular COUNTER data into a single dataframe."""
-    df = UploadCOUNTERReports(sample_COUNTER_report_workbooks).create_dataframe()  #ToDo:: Returns tuple, second part is list of error messages for workbooks and worksheets rejected
+    df, data_not_in_df = UploadCOUNTERReports(sample_COUNTER_report_workbooks).create_dataframe()
     COUNTERData_relation = COUNTERData_relation.drop(columns='report_creation_date')  # Reports ingested using the `UploadCOUNTERReports.create_dataframe()` method have no `report_creation_date` values, so a field for this value containing entirely null values is added to the dataframe returned by the method; thus, the dataframe returned by this method shouldn't have the `report_creation_date` field
+    assert isinstance(data_not_in_df, list)
     assert_frame_equal(df, COUNTERData_relation, check_like=True)  # `check_like` argument allows test to pass if fields aren't in the same order
