5c57e184ba2d7c1e25d5e1b09d3e677590c73638
Elissa Reiskind <ereiskind@fsu.edu>
2023-12-06 17:46:04 -0500

    Fix key error

--- nolcat/view_usage/views.py
+++ nolcat/view_usage/views.py
@@ -61,7 +61,8 @@ def fuzzy_search_on_field(value, field, report):
     df['token_sort_ratio'] = df.apply(lambda record: fuzz.token_sort_ratio(record[field], value), axis='columns')
     df['token_set_ratio'] = df.apply(lambda record: fuzz.token_set_ratio(record[field], value), axis='columns')
     log.debug(f"Dataframe with all fuzzy matching values:\n{df}")
-    log.info(df['Taylor &amp; Francis Ltd'])  #TEST: temp --> KeyError: 'Taylor &amp; Francis Ltd'
+    df2 = df.set_index('publisher')  #TEST: temp
+    log.info(df2['Taylor &amp; Francis Ltd'])  #TEST: temp
     df = df[
         (df['partial_ratio'] >= 70) |
         (df['token_sort_ratio'] >= 65) |
