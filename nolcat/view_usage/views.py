2ed86f61ff25925b2563573f569e86adb7365cfc
ereiskind <ereiskind@fsu.edu>
2023-12-13 16:32:14 -0500

    Further refine logging statements

--- nolcat/view_usage/views.py
+++ nolcat/view_usage/views.py
@@ -234,12 +234,14 @@ def use_predefined_SQL_query():
         #ToDo: What type juggling is needed to ensure numeric string values, integers, and dates are properly formatted in the CSV?
         file_path = Path(__file__).parent / 'NoLCAT_download.csv'
         #TEST: temp
+        test_dict = dict()
         for proc in process_iter():
             log.info(f"`proc` is {proc}")
-            test_dict = dict()
             for attr in list(proc.as_dict().keys()):
-                test_dict[proc] = {k: str(v) for k, v in proc.as_dict(attrs=[attr]).items()}
-            log.info(test_dict)
+                for k, v in proc.as_dict(attrs=[attr]).items():
+                    test_dict[proc][k] = str(v)
+                    log.info(f"Key {proc}, subkey {k}, value {v}")
+        log.info(test_dict)
         #TEST: temp end
         df.to_csv(
             file_path,
@@ -250,12 +252,14 @@ def use_predefined_SQL_query():
         log.info(f"After writing the dataframe to download to a CSV," + check_if_file_exists_statement(file_path, False))
         log.debug(list_folder_contents_statement(Path(__file__).parent))
         #TEST: temp
+        test_dict = dict()
         for proc in process_iter():
             log.info(f"`proc` is {proc}")
-            test_dict = dict()
             for attr in list(proc.as_dict().keys()):
-                test_dict[proc] = {k: str(v) for k, v in proc.as_dict(attrs=[attr]).items()}
-            log.info(test_dict)
+                for k, v in proc.as_dict(attrs=[attr]).items():
+                    test_dict[proc][k] = str(v)
+                    log.info(f"Key {proc}, subkey {k}, value {v}")
+        log.info(test_dict)
         #TEST: temp end
         return redirect(url_for('download_file', file_path=str(file_path)))  #TEST: `ValueError: I/O operation on closed file.` raised on `client.post` in `test_use_predefined_SQL_query_with_COUNTER_standard_views()`; above logging statements got to stdout indicating successful creation of `NoLCAT_download.csv`, but opening logging statement for `download_file()` route function isn't output at all
     else:
