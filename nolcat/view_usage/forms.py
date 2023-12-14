436512e92365242e9bc32977ef0bdbf0bb21c7e5
ereiskind <ereiskind@fsu.edu>
2023-12-14 12:09:59 -0500

    Write DR query wizard page

--- nolcat/view_usage/forms.py
+++ nolcat/view_usage/forms.py
@@ -137,9 +137,9 @@ class DRQueryWizardForm(FlaskForm):
     resource_name_filter = StringField("Enter the name of the database the query should return:", validators=[Optional()])
     publisher_filter = StringField("Enter the name of the publisher the query should return:", validators=[Optional()])
     platform_filter = StringField("Enter the name of the platform the query should return:", validators=[Optional()])
-    data_type_filter = SelectMultipleField("Select all of the data types the query should return (all data types ending in asterisks have yet to be implemented):", choices=data_type_values)  #ToDo: Should all values be leaving this blank?
-    access_method_filter = SelectMultipleField("Select all of the access methods the query should return:", choices=access_method_values)  #ToDo: Should all values be leaving this blank?
-    metric_type_filter = SelectMultipleField("Select all of the metric types the query should return:", choices=metric_type_values)  #ToDo: Should all values be leaving this blank?
+    data_type_filter = SelectMultipleField("Select all of the data types the query should return (all data types ending in asterisks have yet to be implemented):", choices=data_type_values, validators=[DataRequired()])
+    access_method_filter = SelectMultipleField("Select all of the access methods the query should return:", choices=access_method_values, validators=[DataRequired()])
+    metric_type_filter = SelectMultipleField("Select all of the metric types the query should return:", choices=metric_type_values, validators=[DataRequired()])
 
 
 class TRQueryWizardForm(FlaskForm):
