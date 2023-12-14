c1e1d8bea8b0bb46d99a0cde43b02e993f94c8f3
ereiskind <ereiskind@fsu.edu>
2023-12-14 13:31:16 -0500

    Reformat first part of `construct_IR_query_with_wizard`

--- nolcat/view_usage/views.py
+++ nolcat/view_usage/views.py
@@ -648,8 +648,7 @@ def construct_IR_query_with_wizard():
     """Returns a page that allows a valid SQL query for item usage data to be constructed through drop-downs and fuzzy text searches."""
     log.info("Starting `construct_IR_query_with_wizard()`.")
     form = IRQueryWizardForm()
-    if request.method == 'GET':
-        return render_template('view_usage/IR-query-wizard.html', form=form)
+    # Initial rendering of template is in `query_wizard_sort_redirect()`
     if form.validate_on_submit():
         #ALERT: temp
         log.info(f"`begin_date` is {form.begin_date.data} (type {type(form.begin_date.data)})")
@@ -671,59 +670,79 @@ def construct_IR_query_with_wizard():
         log.info(f"`access_method_filter` is {form.access_method_filter.data} (type {type(form.access_method_filter.data)})")
         log.info(f"`metric_type_filter` is {form.metric_type_filter.data} (type {type(form.metric_type_filter.data)})")
         #ALERT: temp end
-        resource_name_filter_options = fuzzy_search_on_field(form.resource_name_filter, "resource_name", "IR")
-        publisher_filter_options = fuzzy_search_on_field(form.publisher_filter, "publisher", "IR")
-        platform_filter_options = fuzzy_search_on_field(form.platform_filter, "platform", "IR")
-        parent_title_filter_options = fuzzy_search_on_field(form.parent_title_filter, "parent_title", "IR")
-        # Any returned SelectField with pipes in it, split on pipes and filter OR for all the given values
-        '''
+        #Section: Create SELECT Statement
+        #Subsection: Create SELECT List
+        #ToDo: "\n".join([f"{field}," for field in form.display_fields])
+        # metric_type,
+        # usage_date,
+        # SUM(usage_count)
+
+        #Subsection: Create WHERE Filters
+        if form.resource_name_filter:
+            resource_name_filter_options = fuzzy_search_on_field(form.resource_name_filter, "resource_name", "IR")
+            resource_name_filter_option_statement = " OR ".join([f"resource_name='{name}'" for name in resource_name_filter_options])
+            resource_name_filter_option_statement = f"AND ({resource_name_filter_option_statement})"
+        else:
+            resource_name_filter_option_statement = ""  # This allows the same f-string query constructor to be used regardless of if there's a `form.resource_name_filter` value
+        
+        if form.publisher_filter:
+            publisher_filter_options = fuzzy_search_on_field(form.publisher_filter, "publisher", "IR")
+            publisher_filter_option_statement = " OR ".join([f"publisher='{name}'" for name in publisher_filter_options])
+            publisher_filter_option_statement = f"AND ({publisher_filter_option_statement})"
+        else:
+            publisher_filter_option_statement = ""  # This allows the same f-string query constructor to be used regardless of if there's a `form.publisher_filter` value
+
+        if form.platform_filter:
+            platform_filter_options = fuzzy_search_on_field(form.platform_filter, "platform", "IR")
+            platform_filter_option_statement = " OR ".join([f"platform='{name}'" for name in platform_filter_options])
+            platform_filter_option_statement = f"AND ({platform_filter_option_statement})"
+        else:
+            platform_filter_option_statement = ""  # This allows the same f-string query constructor to be used regardless of if there's a `form.platform_filter` value
+        
+        if form.parent_title_filter:
+            parent_title_filter_options = fuzzy_search_on_field(form.parent_title_filter, "parent_title", "IR")
+            parent_title_filter_option_statement = " OR ".join([f"parent_title='{name}'" for name in parent_title_filter_options])
+            parent_title_filter_option_statement = f"AND ({parent_title_filter_option_statement})"
+        else:
+            parent_title_filter_option_statement = ""  # This allows the same f-string query constructor to be used regardless of if there's a `form.parent_title_filter` value
+        
+        # For each list, check each item, further divide any items with pipes into their own list items, then combine as in `platform_filter_option_statement`
+            #publication_date
+            #ISBN
+            #print_ISSN OR online_ISSN
+            #parent_ISBN
+            #parent_print_ISSN OR parent_online_ISSN
+            #data_type
+            #YOP
+            #access_type
+            #access_method
+            #metric_type
+
+        #Subsection: Construct SQL Query
+        # A f-string can be used because all of the values are from fixed text fields with program-supplied vocabularies, filtered by restrictive regexes, or derived from values already in the database
+        query = f"""
             SELECT
-            <fields selected to display>,
-                COUNTERData.resource_name
-                COUNTERData.publisher
-                COUNTERData.platform
-                COUNTERData.publication_date
-                COUNTERData.DOI
-                COUNTERData.ISBN
-                COUNTERData.print_ISSN
-                COUNTERData.online_ISSN
-                COUNTERData.parent_title
-                COUNTERData.parent_publication_date
-                COUNTERData.parent_data_type
-                COUNTERData.parent_DOI
-                COUNTERData.parent_ISBN
-                COUNTERData.parent_print_ISSN
-                COUNTERData.parent_online_ISSN
-                COUNTERData.data_type
-                COUNTERData.YOP
-                COUNTERData.access_method
-            COUNTERData.metric_type
-            COUNTERData.usage_date
-            SUM(COUNTERData.usage_count)
-        FROM COUNTERData
-        WHERE
-            COUNTERData.report_type -- PAGE 1
-            COUNTERData.usage_date x2 -- PAGE 1
-            <filter statements>
-                COUNTERData.resource_name (needs fuzzy search)
-                COUNTERData.publisher (less fuzzy search)
-                COUNTERData.platform (less fuzzy search)
-                COUNTERData.publication_date
-                COUNTERData.ISBN
-                COUNTERData.print_ISSN OR COUNTERData.online_ISSN
-                COUNTERData.parent_title (needs fuzzy search)
-                COUNTERData.parent_ISBN
-                COUNTERData.parent_print_ISSN OR COUNTERData.parent_online_ISSN
-                COUNTERData.data_type
-                COUNTERData.YOP
-                COUNTERData.access_type
-                COUNTERData.access_method
-                COUNTERData.metric_type
-        GROUP BY
-            <fields in select not in where or with a grouping function>;
-        '''
-        #ToDo: Construct query, using answers returned by searches
-        # usage_date>='{begin_date.strftime('%Y-%m-%d')}' AND usage_date<='{end_date.strftime('%Y-%m-%d')}'
+                #ToDo: Result of `Create SELECT List` subsection
+            FROM COUNTERData
+            WHERE
+                report_type='IR'
+                AND usage_date>='{form.begin_date.strftime('%Y-%m-%d')}' AND usage_date<='{form.end_date.strftime('%Y-%m-%d')}'
+                {resource_name_filter_option_statement}
+                {publisher_filter_option_statement}
+                {platform_filter_option_statement}
+                {parent_title_filter_option_statement}
+                AND #ToDo: publication_date
+                AND #ToDo: ISBN
+                AND #ToDo: print_ISSN OR online_ISSN
+                AND #ToDo: parent_ISBN
+                AND #ToDo: parent_print_ISSN OR parent_online_ISSN
+                AND #ToDo: data_type
+                AND #ToDo: YOP
+                AND #ToDo: access_type
+                AND #ToDo: access_method
+                AND #ToDo: metric_type
+            GROUP BY usage_count;
+        """
         '''  COPIED FROM `use_predefined_SQL_query()`
         df = query_database(
             query=query,
