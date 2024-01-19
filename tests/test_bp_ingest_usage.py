a46cf152e3cf220fbce1e058b2d4d586850fe5c3
ereiskind <ereiskind@fsu.edu>
2024-01-19 17:12:10 -0500

    Remove index from database check results

--- tests/test_bp_ingest_usage.py
+++ tests/test_bp_ingest_usage.py
@@ -295,9 +295,8 @@ def test_upload_non_COUNTER_reports(engine, client, header_value, non_COUNTER_AU
     
     #Section: Confirm Database Update
     check_database_update = query_database(
-        query=f"SELECT AUCT_statistics_source, AUCT_fiscal_year, collection_status, usage_file_path FROM annualUsageCollectionTracking WHERE AUCT_statistics_source = {non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source} AND AUCT_fiscal_year = {non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year};",
+        query=f"SELECT collection_status, usage_file_path FROM annualUsageCollectionTracking WHERE AUCT_statistics_source = {non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source} AND AUCT_fiscal_year = {non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year};",
         engine=engine,
-        index=['AUCT_statistics_source', 'AUCT_fiscal_year'],
     )
     assert check_database_update.at[0,'collection_status'] == 'Collection complete'  #TEST: KeyError: 0
     assert check_database_update.at[0,'usage_file_path'] == f"{non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source}_{non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year}{path_to_sample_file.suffix}"
