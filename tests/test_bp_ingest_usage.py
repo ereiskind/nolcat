594d6d4bb5351dc3d6103752a7c5985cbb0446df
ereiskind <ereiskind@fsu.edu>
2024-01-19 14:46:41 -0500

    Reorganize test function

--- tests/test_bp_ingest_usage.py
+++ tests/test_bp_ingest_usage.py
@@ -271,7 +271,8 @@ def test_upload_non_COUNTER_reports(engine, client, header_value, non_COUNTER_AU
         encoding='utf-8',
     )
 
-    #Section: Perform Test Actions
+    #Section: Confirm File Upload to S3
+    #Subsection: Perform Test Actions
     header_value['Content-Type'] = form_submissions.content_type
     POST_response = client.post(
         '/ingest_usage/upload-non-COUNTER',
@@ -281,17 +282,27 @@ def test_upload_non_COUNTER_reports(engine, client, header_value, non_COUNTER_AU
         data=form_submissions,
     )  #ToDo: Is a try-except block that retries with a 299 timeout needed?
 
+    #Subsection: Assert Statements
     with open(TOP_NOLCAT_DIRECTORY / 'nolcat' / 'ingest_usage' / 'templates' / 'ingest_usage' / 'index.html', 'br') as HTML_file:
         file_soup = BeautifulSoup(HTML_file, 'lxml')
         HTML_file_title = file_soup.head.title.string.encode('utf-8')
         HTML_file_page_title = file_soup.body.h1.string.encode('utf-8')
+    assert POST_response.history[0].status == "302 FOUND"  # This confirms there was a redirect
+    assert POST_response.status == "200 OK"
+    assert HTML_file_title in POST_response.data
+    assert HTML_file_page_title in POST_response.data
+    assert re.search(r"Usage file for .*--FY \d{4} uploaded successfully\.", prepare_HTML_page_for_comparison(POST_response.data))
     
+    #Section: Confirm Database Update
     check_database_update = query_database(
-        query=f"SELECT collection_status, usage_file_path FROM annualUsageCollectionTracking WHERE AUCT_statistics_source = {non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source} AND AUCT_fiscal_year = {non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year};",
+        query=f"SELECT AUCT_statistics_source, AUCT_fiscal_year, collection_status, usage_file_path FROM annualUsageCollectionTracking WHERE AUCT_statistics_source = {non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source} AND AUCT_fiscal_year = {non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year};",
         engine=engine,
         index=['AUCT_statistics_source', 'AUCT_fiscal_year'],
     )  #TEST: 0::Running the query `SELECT collection_status, usage_file_path FROM annualUsageCollectionTracking WHERE AUCT_statistics_source = 5 AND AUCT_fiscal_year = 5;` raised the error "None of ['AUCT_statistics_source', 'AUCT_fiscal_year'] are in the columns".
+    assert check_database_update.at[0,'collection_status'] == 'Collection complete'  #TEST: 0::AttributeError: 'str' object has no attribute 'at'
+    assert check_database_update.at[0,'usage_file_path'] == f"{non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source}_{non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year}{path_to_sample_file.suffix}"
 
+    #Section: Check S3 for File
     list_objects_response = s3_client.list_objects_v2(
         Bucket=BUCKET_NAME,
         Prefix=f"{PATH_WITHIN_BUCKET}{non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source}_{non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year}",
@@ -303,13 +314,4 @@ def test_upload_non_COUNTER_reports(engine, client, header_value, non_COUNTER_AU
     for contents_dict in list_objects_response['Contents']:  #TEST: 1::KeyError: 'Contents'
         bucket_contents.append(contents_dict['Key'])
     bucket_contents = [file_name.replace(f"{PATH_WITHIN_BUCKET}", "") for file_name in bucket_contents]
-
-    #Section: Assert Statements
-    assert POST_response.history[0].status == "302 FOUND"  # This confirms there was a redirect
-    assert POST_response.status == "200 OK"
-    assert HTML_file_title in POST_response.data
-    assert HTML_file_page_title in POST_response.data
-    assert check_database_update.at[0,'collection_status'] == 'Collection complete'  #TEST: 0::AttributeError: 'str' object has no attribute 'at'
-    assert check_database_update.at[0,'usage_file_path'] == f"{non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source}_{non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year}{path_to_sample_file.suffix}"
-    #ToDo: ingest_usage.views.upload_non_COUNTER_reports() flash message after validate_on_submit  in post_response.data
     assert f"{non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source}_{non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year}{path_to_sample_file.suffix}" in bucket_contents
