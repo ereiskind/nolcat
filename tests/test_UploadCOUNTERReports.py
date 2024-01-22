a1eb562020b00c588f0976d25922135a448a10ca
ereiskind <ereiskind@fsu.edu>
2024-01-22 11:32:27 -0500

    Name index in test

--- tests/test_UploadCOUNTERReports.py
+++ tests/test_UploadCOUNTERReports.py
@@ -37,5 +37,6 @@ def test_create_dataframe(sample_COUNTER_report_workbooks, COUNTERData_relation)
     """Tests transforming multiple Excel workbooks with tabular COUNTER data into a single dataframe."""
     df, data_not_in_df = UploadCOUNTERReports(sample_COUNTER_report_workbooks).create_dataframe()
     COUNTERData_relation = COUNTERData_relation.drop(columns='report_creation_date')  # Reports ingested using the `UploadCOUNTERReports.create_dataframe()` method have no `report_creation_date` values, so a field for this value containing entirely null values is added to the dataframe returned by the method; thus, the dataframe returned by this method shouldn't have the `report_creation_date` field
+    df.index.name = "COUNTER_data_ID"  # The result of the `UploadCOUNTERReports.create_dataframe()` method doesn't name the index, as it is overwritten soon after
     assert isinstance(data_not_in_df, list)
     assert_frame_equal(df, COUNTERData_relation, check_like=True)  # `check_like` argument allows test to pass if fields aren't in the same order
