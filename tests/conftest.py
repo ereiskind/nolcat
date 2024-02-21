30fc5f4cd23cc647e34aac5c15d4f514e2616b90
ereiskind <ereiskind@fsu.edu>
2024-02-21 17:05:59 -0500

    Remove keyword parameter from remove_file_from_S3

--- tests/conftest.py
+++ tests/conftest.py
@@ -277,24 +277,19 @@ def path_to_sample_file(request):
 
 
 @pytest.fixture
-def remove_file_from_S3(path_to_sample_file, non_COUNTER_AUCT_object_before_upload=None):
+def remove_file_from_S3(path_to_sample_file):
     """Removes a file loaded into S3.
 
-    This fixture creates a name for the file in S3 based either the name of the file or, in the case of the `AnnualUsageCollectionTracking.upload_nonstandard_usage_file()` method, a combination of the file name and class attributes. The yield is a null value as no data is needed from it; the teardown operations using the previously determined file name is the primary purpose of this fixture.
+    The yield is a null value as no data is needed from it; the teardown operations using the previously determined file name is the primary purpose of this fixture.
 
     Args:
         path_to_sample_file (pathlib.Path): an absolute file path to a randomly selected file
-        non_COUNTER_AUCT_object_before_upload (nolcat.models.AnnualUsageCollectionTracking, optional): an AnnualUsageCollectionTracking object corresponding to a record which can have a non-COUNTER usage file uploaded; default is `None`
 
     Yields:
         None
     """
     log.debug(fixture_variable_value_declaration_statement("path_to_sample_file", path_to_sample_file))
-    if non_COUNTER_AUCT_object_before_upload:
-        log.debug(fixture_variable_value_declaration_statement("non_COUNTER_AUCT_object_before_upload", non_COUNTER_AUCT_object_before_upload))
-        file_name = f"{non_COUNTER_AUCT_object_before_upload.AUCT_statistics_source}_{non_COUNTER_AUCT_object_before_upload.AUCT_fiscal_year}{path_to_sample_file.suffix}"
-    else:
-        file_name = f"test_{path_to_sample_file.name}"
+    file_name = f"test_{path_to_sample_file.name}"
     log.info(fixture_variable_value_declaration_statement("file_name", file_name))
     yield None
     try:
