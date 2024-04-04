29ed945df617d9b2906c145af3d846e858845700
ereiskind <ereiskind@fsu.edu>
2024-04-04 16:07:49 -0400

    Add key/field name to form submission value

--- tests/test_bp_ingest_usage.py
+++ tests/test_bp_ingest_usage.py
@@ -45,7 +45,7 @@ def test_upload_COUNTER_data_via_Excel(engine, client, header_value, COUNTERData
     caplog.set_level(logging.INFO, logger='nolcat.convert_JSON_dict_to_dataframe')  # For `create_dataframe()`
     caplog.set_level(logging.INFO, logger='nolcat.app')  # For `first_new_PK_value()` and `query_database()`
     
-    form_submissions = [open(file, 'rb') for file in Path(TOP_NOLCAT_DIRECTORY, 'tests', 'bin', 'COUNTER_workbooks_for_tests').iterdir()]
+    form_submissions = {'COUNTER_data': [open(file, 'rb') for file in Path(TOP_NOLCAT_DIRECTORY, 'tests', 'bin', 'COUNTER_workbooks_for_tests').iterdir()]}
     log.debug(f"The files being uploaded to the database are:\n{form_submissions}")
     header_value['Content-Type'] = 'multipart/form-data'
     
