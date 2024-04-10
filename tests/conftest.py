016534a6562c9cc271d67022c02478dfdc3750eb
ereiskind <ereiskind@fsu.edu>
2024-04-10 13:00:11 -0400

    Create `workbook_0_2019_relation`

--- tests/conftest.py
+++ tests/conftest.py
@@ -282,6 +282,16 @@ def workbook_2_2018_relation():
 
 
 @pytest.fixture
+def workbook_0_2019_relation():
+    """Creates a dataframe of test data based on the COUNTER data in the `0_2019.xlsx` workbook.
+    
+    Yields:
+        dataframe: a relation of test data
+    """
+    yield relations.workbook_0_2019_relation()
+
+
+@pytest.fixture
 def COUNTERData_relation():
     """Creates a dataframe that can be loaded into the `COUNTERData` relation.
     
