bfc49710ca7b55c9a87d4f5c226e3e9538df2632
ereiskind <ereiskind@fsu.edu>
2024-04-12 17:18:34 -0400

    Update output for temp logging statements

--- tests/test_UploadCOUNTERReports.py
+++ tests/test_UploadCOUNTERReports.py
@@ -94,13 +94,17 @@ def test_create_dataframe(sample_COUNTER_report_workbooks, COUNTERData_relation)
         if z[0][1] is pd.NA and z[1][1] is pd.NA:
             continue  # Both null values are `pd.NA`
         else:
-            if records_set.get(z[0][0][0]):
-                records_set[z[0][0][0]].append((z[0][0][1], z[1][0][1]))
-            else:
-                records_set[z[0][0][0]] = [(z[0][0][1], z[1][0][1])]
-    for k, v in records_set.items():
-        log.warning(f"`k` is {k} and `v` is {v}")
-        #log.warning(f"`x_df.iloc` is {x_df.iloc[]}")
-        #log.warning(f"`y_df.iloc` is {y_df.iloc[]}")
+            log.warning(f"`z` is {z}")
+            log.warning(f"`z[0]` is {z[0]}")
+            log.warning(f"`z[0][0]` is {z[0][0]}")
+            log.warning(f"`z[0][0][0]` is {z[0][0][0]}")
+    #        if records_set.get(z[0][0][0]):
+    #            records_set[z[0][0][0]].append((z[0][0][1], z[1][0][1]))
+    #        else:
+    #            records_set[z[0][0][0]] = [(z[0][0][1], z[1][0][1])]
+    #for k, v in records_set.items():
+    #    log.warning(f"`k` is {k} and `v` is {v}")
+    #    #log.warning(f"`x_df.iloc` is {x_df.iloc[]}")
+    #    #log.warning(f"`y_df.iloc` is {y_df.iloc[]}")
     #TEST: end temp
     assert_frame_equal(df, COUNTERData_relation[df.columns.tolist()])
