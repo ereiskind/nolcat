e3ba45536c291c7532e90a472b05daa248f74c85
ereiskind <ereiskind@fsu.edu>
2024-04-12 10:39:54 -0400

    Correct sorting method used

--- tests/test_UploadCOUNTERReports.py
+++ tests/test_UploadCOUNTERReports.py
@@ -47,7 +47,7 @@ def test_create_dataframe(sample_COUNTER_report_workbooks, COUNTERData_relation)
         log.warning(f"Dataframe compare with keep_equal:\n{df.compare(COUNTERData_relation, keep_equal=True)}")
     except:
         log.warning(f"Fields are equal: {df.columns == COUNTERData_relation.columns}")
-        log.warning(f"Sorted fields are equal: {df.columns.sort() == COUNTERData_relation.columns.sort()}")
+        log.warning(f"Sorted fields are equal: {df.columns.sort_values() == COUNTERData_relation.columns.sort_values()}")
         log.warning(f"Record indexes are equal: {df.index == COUNTERData_relation.index}")
     #TEST: end temp
     assert_frame_equal(df, COUNTERData_relation, check_like=True)  # `check_like` argument allows test to pass if fields aren't in the same order
