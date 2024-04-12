1851ec5e0b3768dd27ef8ab7776aa824992828ce
ereiskind <ereiskind@fsu.edu>
2024-04-12 12:54:12 -0400

    Enhance temp logging statements

--- tests/test_UploadCOUNTERReports.py
+++ tests/test_UploadCOUNTERReports.py
@@ -61,12 +61,17 @@ def test_create_dataframe(sample_COUNTER_report_workbooks, COUNTERData_relation)
     assert isinstance(data_not_in_df, list)
     #TEST: temp
     import numpy as np
-    x = [df.iloc[i,j] for i,j in zip(*np.where(pd.isnull(df)))]
-    log.warning(f"`df` has {len(x)} null values")
+    x_df = df[(df.publication_date.isnull()) | (df.parent_publication_date.isnull())]
     y_df = COUNTERData_relation[df.columns.tolist()]
+    y_df = y_df[(y_df.publication_date.isnull()) | (y_df.parent_publication_date.isnull())]
+    log.warning(return_string_of_dataframe_info(x_df))
+    log.warning(return_string_of_dataframe_info(y_df))
+    log.warning(f"Complete Compare:\n{x_df.compare(y_df)}")
+    x = [x_df.iloc[i,j] for i,j in zip(*np.where(pd.isnull(x_df)))]
     y = [y_df.iloc[i,j] for i,j in zip(*np.where(pd.isnull(y_df)))]
-    log.warning(f"`COUNTERData_relation` has {len(y)} null values")
     for z in list(enumerate(zip(x,y))):
-        log.warning(f"Null values {int(z[0]) + 1} are {z[1][0]} and {z[1][1]}")
+        log.warning(f"`z` is {z}")
+        log.warning(f"`x_df.iloc` is {x_df.iloc[z[0]]}")
+        log.warning(f"`y_df.iloc` is {y_df.iloc[z[0]]}")
     #TEST: end temp
     assert_frame_equal(df, COUNTERData_relation[df.columns.tolist()])
