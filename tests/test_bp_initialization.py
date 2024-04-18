d791d9e885b52ef4fb3124e227f8b52000b097c3
ereiskind <ereiskind@fsu.edu>
2024-04-18 11:09:25 -0400

    Start cycling through `form_submissions` options

--- tests/test_bp_initialization.py
+++ tests/test_bp_initialization.py
@@ -574,20 +574,21 @@ def test_collect_AUCT_and_historical_COUNTER_data(engine, client, tmp_path, head
     
     #Section: Submit Forms via HTTP POST
     #TEST: temp
+    from conftest import mock_FileStorage_object
     f1 = create_COUNTERData_workbook_iterdir_list[0]
     f2 = create_COUNTERData_workbook_iterdir_list[1]
-    #TEST: end temp
     form_submissions = MultipartEncoder(
         fields={
             'annualUsageCollectionTracking_CSV': ('annualUsageCollectionTracking_relation.csv', open(tmp_path / 'annualUsageCollectionTracking_relation.csv', 'rb'), 'text/csv'),
             'COUNTER_reports': (
-                (f1.name, open(f1, 'rb'), 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'),  #TEST: temp
-                (f2.name, open(f2, 'rb'), 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'),  #TEST: temp
+                mock_FileStorage_object(f1),
+                mock_FileStorage_object(f2),
             ),
-            #ToDo: The `UploadCOUNTERReports` constructor is looking for a list of Werkzeug FileStorage object(s); can this be used to the advantage of the test?
         },
         encoding='utf-8',
     )
+    #TEST: end temp
+    #ToDo: The `UploadCOUNTERReports` constructor is looking for a list of Werkzeug FileStorage object(s); can this be used to the advantage of the test?
     log.warning(f"`form_submissions`:\n{form_submissions}")  #TEST: temp
     header_value['Content-Type'] = 'multipart/form-data'
     POST_response = client.post(
