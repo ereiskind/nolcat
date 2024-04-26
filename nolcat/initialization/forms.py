34522d0a62d85ac8f5d55512e68ba4203b6f4441
ereiskind <ereiskind@fsu.edu>
2024-04-26 09:31:32 -0400

    Create `HistoricalNonCOUNTERForm`

--- nolcat/initialization/forms.py
+++ nolcat/initialization/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms.fields import MultipleFileField
+from wtforms.fields import FieldList
+from wtforms.fields import FormField
 from wtforms.validators import DataRequired
 
 
@@ -27,9 +29,17 @@ class AUCTAndCOUNTERForm(FlaskForm):
     COUNTER_reports = MultipleFileField("Select the COUNTER report workbooks. If all the files are in a single folder and that folder contains no other items, navigate to that folder, then use `Ctrl + a` to select all the files in the folder.")
 
 
-class HistoricalNonCOUNTERForm(FlaskForm):
+class HistoricalNonCOUNTERFormField(FlaskForm):
     """Creates a form field for uploading a file containing non-COUNTER usage data.
     
-    This form is actually created through iterating this field for each statistics source and fiscal year combination calling for non-COUNTER usage data.
+    This field is called iteratively in `HistoricalNonCOUNTERForm` to create a form. This iterative form setup is taken from https://stackoverflow.com/a/28378186.
     """
     usage_file = FileField("Select the file containing the usage statistics for the statistics source and fiscal year stated above.")
+
+
+class HistoricalNonCOUNTERForm(FlaskForm):
+    """Creates a form by iterating the `HistoricalNonCOUNTERFormField` field.
+    
+    This form is created through an iteration of the `HistoricalNonCOUNTERFormField` field for each statistics source and fiscal year combination calling for non-COUNTER usage data. This iterative form setup is taken from https://stackoverflow.com/a/28378186.
+    """
+    usage_files = FieldList(FormField(HistoricalNonCOUNTERFormField))
