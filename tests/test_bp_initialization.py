f436658a29213c31e435c008782a7d145e710e8d
ereiskind <ereiskind@fsu.edu>
2024-04-30 17:35:15 -0400

    Correct source of files used in test function

--- tests/test_bp_initialization.py
+++ tests/test_bp_initialization.py
@@ -709,9 +709,9 @@ def files_for_test_upload_historical_non_COUNTER_usage(tmp_path, caplog):
     number_of_uploads = random.randint(2, int(df.iloc[0][0]))
     JSON_files = random.choices([file for file in Path(TOP_NOLCAT_DIRECTORY, 'tests', 'data', 'COUNTER_JSONs_for_tests').iterdir()], k=number_of_uploads//2)
     if number_of_uploads % 2 == 1:
-        Excel_files = random.choices([file for file in Path(TOP_NOLCAT_DIRECTORY, 'tests', 'bin', 'sample_COUNTER_R4_reports').iterdir()], k=(number_of_uploads//2)+1)
+        Excel_files = random.choices([file for file in Path(TOP_NOLCAT_DIRECTORY, 'tests', 'bin', 'COUNTER_workbooks_for_tests').iterdir()], k=(number_of_uploads//2)+1)
     else:
-        Excel_files = random.choices([file for file in Path(TOP_NOLCAT_DIRECTORY, 'tests', 'bin', 'sample_COUNTER_R4_reports').iterdir()], k=number_of_uploads//2)
+        Excel_files = random.choices([file for file in Path(TOP_NOLCAT_DIRECTORY, 'tests', 'bin', 'COUNTER_workbooks_for_tests').iterdir()], k=number_of_uploads//2)
     original_files_to_upload = JSON_files + Excel_files
     log.debug(fixture_variable_value_declaration_statement("original_files_to_upload", original_files_to_upload))
 
