e808a59adf7bec5d431e3d008498dc9260809a57
ereiskind <ereiskind@fsu.edu>
2024-05-24 09:53:52 -0400

    Correct temp logging statement

--- nolcat/models.py
+++ nolcat/models.py
@@ -1679,7 +1679,7 @@ class AnnualUsageCollectionTracking(db.Model):
         #Section: Use Temp File to Upload File to S3
         temp_file_path = TOP_NOLCAT_DIRECTORY / 'nolcat' / f'temp{file_extension}'
         file.save(temp_file_path)
-        log.warning(f"iterdir for parent of {file} immediately after saving:\n{format_list_for_stdout(file.parent.iterdir())}")  #TEST: temp
+        log.warning(f"iterdir for parent of {temp_file_path} immediately after saving:\n{format_list_for_stdout(temp_file_path.parent.iterdir())}")  #TEST: temp
         logging_message = upload_file_to_S3_bucket(
             temp_file_path,
             file_name,
