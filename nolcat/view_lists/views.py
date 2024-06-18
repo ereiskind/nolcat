e7ad17d96767934e3d0d4cd5e321408a59ec226c
ereiskind <ereiskind@fsu.edu>
2024-06-18 11:09:18 -0400

    Add single quotes around PK value in `url_for`

--- nolcat/view_lists/views.py
+++ nolcat/view_lists/views.py
@@ -103,8 +103,8 @@ def view_lists_homepage(list):
         flash(message)
         return abort(404)
     
-    df['View Record Details'] = df['ID'].apply(lambda cell_value: f"url_for('view_lists.view_list_record', list='{list}', PK={cell_value})")
-    df['Edit'] = df['ID'].apply(lambda cell_value: f"url_for('view_lists.edit_list_record', list='{list}', PK={cell_value})")
+    df['View Record Details'] = df['ID'].apply(lambda cell_value: f"url_for('view_lists.view_list_record', list='{list}', PK='{cell_value}')")
+    df['Edit'] = df['ID'].apply(lambda cell_value: f"url_for('view_lists.edit_list_record', list='{list}', PK='{cell_value}')")
     log.info(f"List of {list} as dataframe:\n{df}")
     return render_template(
         'view_lists/index.html',
