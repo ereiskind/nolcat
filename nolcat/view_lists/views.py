d6896dd63155cd1ead95384c5db6bcd911c4fe2f
ereiskind <ereiskind@fsu.edu>
2024-06-18 09:44:07 -0400

    Isolate individual PK values by record

--- nolcat/view_lists/views.py
+++ nolcat/view_lists/views.py
@@ -102,8 +102,8 @@ def view_lists_homepage(list):
         flash(message)
         return abort(404)
     
-    df['View Record Details'] = f"url_for('view_lists.view_list_record', list='{list}', PK={df['ID']})"
-    df['Edit'] = f"url_for('view_lists.edit_list_record', list='{list}', PK={df['ID']})"
+    df['View Record Details'] = f"url_for('view_lists.view_list_record', list='{list}', PK={df['ID'].apply(lambda cell_value: cell_value)})"
+    df['Edit'] = f"url_for('view_lists.edit_list_record', list='{list}', PK={df['ID'].apply(lambda cell_value: cell_value)})"
     return render_template(
         'view_lists/index.html',
         title=title,
