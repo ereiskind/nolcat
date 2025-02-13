7f45db236ab0b304c57ce03cf2255b9360cb455c
ereiskind <ereiskind@fsu.edu>
2025-02-13 12:17:36 -0500

    Remove unneeded temp log statements

--- tests/test_ConvertJSONDictToDataframe.py
+++ tests/test_ConvertJSONDictToDataframe.py
@@ -5481,8 +5481,6 @@ def test_create_dataframe_from_R5b1_DR(sample_SUSHI_DR_response_R5b1_JSON_dict, 
 def test_create_dataframe_from_R5b1_TR(sample_SUSHI_TR_response_R5b1_JSON_dict, sample_SUSHI_TR_response_R5b1_dataframe):
     """Tests transforming dictionaries derived from R5.1 SUSHI TR JSONs into dataframes."""
     df = ConvertJSONDictToDataframe(sample_SUSHI_TR_response_R5b1_JSON_dict).create_dataframe()
-    temp = df.compare(sample_SUSHI_TR_response_R5b1_dataframe[df.columns.tolist()], result_names=('from_fx', 'fixture'))  #TEST: temp
-    temp.to_csv(TOP_NOLCAT_DIRECTORY / "TR.csv", index_label="Index")  #TEST: temp
     assert_frame_equal(df, sample_SUSHI_TR_response_R5b1_dataframe[df.columns.tolist()])
 
 
