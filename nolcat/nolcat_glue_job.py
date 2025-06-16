9992b2c283bd35ba37472dde9329b465f5f46d7f
ereiskind <ereiskind@fsu.edu>
2025-06-16 18:05:15 -0400

    Frame Glue job file

--- nolcat/nolcat_glue_job.py
+++ nolcat/nolcat_glue_job.py
@@ -1,1 +1,122 @@
-import sys
+# imports here
+
+from .logging_config import *
+
+#ALERT: Move constants here
+
+
+log = logging.getLogger(__name__)
+
+
+#SECTION: Basic Helper Functions
+# app.last_day_of_month
+
+# app.ISSN_regex
+
+# app.ISBN_regex
+
+# app.AWS_timestamp_format
+
+# app.non_COUNTER_file_name_regex
+
+# app.empty_string_regex
+
+#SUBSECTION: Formatting Changes
+# app.return_string_of_dataframe_info
+
+# statements.format_list_for_stdout
+
+# statements.remove_IDE_spacing_from_statement
+
+# app.truncate_longer_lines
+
+#SUBSECTION: SUSHI Statements and Regexes
+# statements.unable_to_convert_SUSHI_data_to_dataframe_statement
+
+# statements.successful_SUSHI_call_statement
+
+# statements.harvest_R5_SUSHI_success_statement
+
+# statements.failed_SUSHI_call_statement
+
+# statements.no_data_returned_by_SUSHI_statement
+
+# statements.attempted_SUSHI_call_with_invalid_dates_statement
+
+# statements.reports_with_no_usage_regex
+
+# statements.skip_test_due_to_SUSHI_error_regex
+
+#SUBSECTION: Testing and Error Statements
+# statements.fixture_variable_value_declaration_statement
+
+# statements.Flask_error_statement
+
+# statements.database_function_skip_statements
+
+#SUBSECTION: File Download Statements
+# statements.file_IO_statement
+
+# statements.list_folder_contents_statement
+
+# statements.check_if_file_exists_statement
+
+
+#SECTION: Database and Dataframe Functions
+#SUBSECTION: MySQL Interaction Result Statements
+# statements.database_query_fail_statement
+
+# statements.return_value_from_query_statement
+
+# statements.initialize_relation_class_object_statement
+
+# statements.unable_to_get_updated_primary_key_values_statement
+
+# statements.return_dataframe_from_query_statement
+
+# statements.database_update_fail_statement
+
+# statements.add_data_success_and_update_database_fail_statement
+
+#SUBSECTION: Result Statement Regexes
+# statements.load_data_into_database_success_regex
+
+# statements.update_database_success_regex
+
+#SUBSECTION: Common Dataframe Adjustments
+# app.change_single_field_dataframe_into_series
+
+# app.restore_boolean_values_to_boolean_field
+
+# app.create_AUCT_SelectField_options
+
+# app.extract_value_from_single_value_df
+
+#SUBSECTION: MySQL Interaction
+# app.load_data_into_database
+
+# app.query_database
+
+# app.first_new_PK_value
+
+# app.check_if_data_already_in_COUNTERData
+
+# app.update_database
+
+
+#SECTION: S3 Interaction
+#SUBSECTION: S3 Interaction Statements
+# statements.failed_upload_to_S3_statement
+
+# statements.unable_to_delete_test_file_in_S3_statement
+
+# statements.upload_file_to_S3_bucket_success_regex
+
+# statements.upload_nonstandard_usage_file_success_regex
+
+#SUBSECTION: S3 Interaction Functions
+# statements.file_extensions_and_mimetypes
+
+# app.upload_file_to_S3_bucket
+
+# app.save_unconverted_data_via_upload
