7dd9ed7a98d2081641235fc63d87343333b1b85b
ereiskind <ereiskind@fsu.edu>
2025-06-30 11:05:26 -0400

    Correct spacing between functions

--- nolcat/nolcat_glue_job.py
+++ nolcat/nolcat_glue_job.py
@@ -107,6 +107,7 @@ Make call function with call to database including or after `last_day_of_month`;
     test_check_if_data_in_database_yes -> current_month_like_most_recent_month_with_usage -> last_day_of_month
 '''
 
+
 def ISSN_regex():
     """A regex object matching an ISSN.
 
@@ -115,51 +116,73 @@ def ISSN_regex():
     """
     return re.compile(r"\d{4}\-\d{3}[\dxX]\s*")
 
+
 # app.ISBN_regex
+
 
 # app.AWS_timestamp_format
 
+
 # app.non_COUNTER_file_name_regex
 
+
 # app.empty_string_regex
+
 
 #SUBSECTION: Formatting Changes
 # app.return_string_of_dataframe_info
 
+
 # statements.format_list_for_stdout
+
 
 # statements.remove_IDE_spacing_from_statement
 
+
 # app.truncate_longer_lines
+
 
 #SUBSECTION: SUSHI Statements and Regexes
 # statements.unable_to_convert_SUSHI_data_to_dataframe_statement
 
+
 # statements.successful_SUSHI_call_statement
+
 
 # statements.harvest_R5_SUSHI_success_statement
 
+
 # statements.failed_SUSHI_call_statement
+
 
 # statements.no_data_returned_by_SUSHI_statement
 
+
 # statements.attempted_SUSHI_call_with_invalid_dates_statement
+
 
 # statements.reports_with_no_usage_regex
 
+
 # statements.skip_test_due_to_SUSHI_error_regex
+
 
 #SUBSECTION: Testing and Error Statements
 # statements.fixture_variable_value_declaration_statement
 
+
 # statements.Flask_error_statement
 
+
 # statements.database_function_skip_statements
+
 
 #SUBSECTION: File Download Statements
 # statements.file_IO_statement
 
+
 # statements.list_folder_contents_statement
+
 
 # statements.check_if_file_exists_statement
 
@@ -168,40 +191,57 @@ def ISSN_regex():
 #SUBSECTION: MySQL Interaction Result Statements
 # statements.database_query_fail_statement
 
+
 # statements.return_value_from_query_statement
+
 
 # statements.initialize_relation_class_object_statement
 
+
 # statements.unable_to_get_updated_primary_key_values_statement
+
 
 # statements.return_dataframe_from_query_statement
 
+
 # statements.database_update_fail_statement
 
+
 # statements.add_data_success_and_update_database_fail_statement
+
 
 #SUBSECTION: Result Statement Regexes
 # statements.load_data_into_database_success_regex
 
+
 # statements.update_database_success_regex
+
 
 #SUBSECTION: Common Dataframe Adjustments
 # app.change_single_field_dataframe_into_series
 
+
 # app.restore_boolean_values_to_boolean_field
+
 
 # app.create_AUCT_SelectField_options
 
+
 # app.extract_value_from_single_value_df
+
 
 #SUBSECTION: MySQL Interaction
 # app.load_data_into_database
 
+
 # app.query_database
+
 
 # app.first_new_PK_value
 
+
 # app.check_if_data_already_in_COUNTERData
+
 
 # app.update_database
 
@@ -210,15 +250,21 @@ def ISSN_regex():
 #SUBSECTION: S3 Interaction Statements
 # statements.failed_upload_to_S3_statement
 
+
 # statements.unable_to_delete_test_file_in_S3_statement
+
 
 # statements.upload_file_to_S3_bucket_success_regex
 
+
 # statements.upload_nonstandard_usage_file_success_regex
+
 
 #SUBSECTION: S3 Interaction Functions
 # statements.file_extensions_and_mimetypes
 
+
 # app.upload_file_to_S3_bucket
+
 
 # app.save_unconverted_data_via_upload
--- tests/test_nolcat_glue_job.py
+++ tests/test_nolcat_glue_job.py
@@ -29,21 +29,30 @@ def test_ISSN_regex():
     assert ISSN_regex().fullmatch("1123-000x") is not None
     assert ISSN_regex().fullmatch("0987-6543 ") is not None
 
+
 # test_app.test_ISBN_regex
+
 
 # test_app.test_AWS_timestamp_format
 
+
 # test_app.test_non_COUNTER_file_name_regex
+
 
 # test_app.test_empty_string_regex
 
+
 #ToDo: Create test for `nolcat.app.return_string_of_dataframe_info`
+
 
 # test_statements.test_format_list_for_stdout_with_list
 
+
 # test_statements.test_format_list_for_stdout_with_generator
 
+
 # test_statements.test_remove_IDE_spacing_from_statement
+
 
 # test_app.test_truncate_longer_lines
 
@@ -51,9 +60,12 @@ def test_ISSN_regex():
 #SECTION: Dataframe Adjustment Tests
 # test_app.test_change_single_field_dataframe_into_series
 
+
 # test_app.test_restore_boolean_values_to_boolean_field
 
+
 # test_app.test_create_AUCT_SelectField_options
+
 
 # test_app.test_extract_value_from_single_value_df
 
@@ -61,21 +73,30 @@ def test_ISSN_regex():
 #SECTION: MySQL Interaction Tests
 # test_app.test_SQLAlchemy_engine_creation
 
+
 # test_app.test_load_data_into_database
+
 
 # test_app.test_loading_connected_data_into_other_relation
 
+
 # test_app.test_query_database
+
 
 # test_app.test_first_new_PK_value
 
+
 # Testing of `nolcat.app.check_if_data_already_in_COUNTERData()` in `tests.test_StatisticsSources.test_check_if_data_already_in_COUNTERData()`
+
 
 # test_app.vendors_relation_after_test_update_database
 
+
 # test_app.test_update_database
 
+
 # test_app.vendors_relation_after_test_update_database_with_insert_statement
+
 
 # test_app.test_update_database_with_insert_statement
 
@@ -83,6 +104,8 @@ def test_ISSN_regex():
 #SECTION: S3 Interaction Tests
 # test_app.test_upload_file_to_S3_bucket
 
+
 # test_app.file_name_stem_and_data
+
 
 # test_app.test_save_unconverted_data_via_upload
