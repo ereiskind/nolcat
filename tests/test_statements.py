df917f7be30edc4edb2e2341930c420b73a58fd1
ereiskind <ereiskind@fsu.edu>
2025-06-30 17:38:05 -0400

    Move `test_format_list_for_stdout_with_generator`

--- tests/test_statements.py
+++ tests/test_statements.py
@@ -12,29 +12,6 @@ from nolcat.statements import *
 log = logging.getLogger(__name__)
 
 
-def test_format_list_for_stdout_with_generator(create_COUNTERData_workbook_iterdir_list):
-    """Test pretty printing a list created by a generator object by adding a line break between each item.
-    
-    The assert statements, which look for every file that should be in the created string and then check that there are only that many items in the string, is used to compensate for `iterdir()` not outputting the files in an exact order.
-    """
-    result = format_list_for_stdout(create_COUNTERData_workbook_iterdir_list)
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/0_2017.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/0_2018.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/0_2019.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/0_2020.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/1_2017.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/1_2018.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/1_2019.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/1_2020.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/2_2017.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/2_2018.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/2_2019.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/2_2020.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/3_2019.xlsx" in result
-    assert "/nolcat/tests/bin/COUNTER_workbooks_for_tests/3_2020.xlsx" in result
-    assert len(result.split('\n')) == 14
-
-
 def test_remove_IDE_spacing_from_statement():
     """Test removing newlines and indentations from SQL statements."""
     statement = """
