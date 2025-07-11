21f4f70f7509c9f1dae2e2f69c0cc186c38e9144
ereiskind <ereiskind@fsu.edu>
2025-07-11 11:31:38 -0400

    Move `file_extensions_and_mimetypes`

--- nolcat/nolcat_glue_job.py
+++ nolcat/nolcat_glue_job.py
@@ -2615,7 +2615,32 @@ Called only one time
 
 
 #SUBSECTION: S3 Interaction Functions
-# statements.file_extensions_and_mimetypes
+def file_extensions_and_mimetypes():
+    """A dictionary of the file extensions for the types of files that can be downloaded to S3 via NoLCAT and their mimetypes.
+    
+    This helper function is called in `create_app()` and thus must be before that function.
+
+    Returns:
+        dict: the file extensions and their corresponding mimetypes
+    """
+    return {
+        ".xlsx": "application/vnd.ms-excel",
+        ".csv": "text/csv",
+        ".tsv": "text/tab-separated-values",
+        ".pdf": "application/pdf",
+        ".docx": "application/msword",
+        ".pptx": "application/vnd.ms-powerpoint",
+        ".txt": "text/plain",
+        ".jpeg": "image/jpeg",
+        ".jpg":"image/jpeg",
+        ".png": "image/png",
+        ".svg": "image/svg+xml",
+        ".json": "application/json",
+        ".html": "text/html",
+        ".htm": "text/html",
+        ".xml": "text/xml",
+        ".zip": "application/zip",
+    }
 
 
 # app.upload_file_to_S3_bucket
--- nolcat/statements.py
+++ nolcat/statements.py
@@ -8,26 +8,3 @@ import re
 #Section: Simple Helper Functions
 # These are helper functions that don't work as well in `nolcat.app` for various reasons
 
-def file_extensions_and_mimetypes():
-    """A dictionary of the file extensions for the types of files that can be downloaded to S3 via NoLCAT and their mimetypes.
-    
-    This helper function is called in `create_app()` and thus must be before that function.
-    """
-    return {
-        ".xlsx": "application/vnd.ms-excel",
-        ".csv": "text/csv",
-        ".tsv": "text/tab-separated-values",
-        ".pdf": "application/pdf",
-        ".docx": "application/msword",
-        ".pptx": "application/vnd.ms-powerpoint",
-        ".txt": "text/plain",
-        ".jpeg": "image/jpeg",
-        ".jpg":"image/jpeg",
-        ".png": "image/png",
-        ".svg": "image/svg+xml",
-        ".json": "application/json",
-        ".html": "text/html",
-        ".htm": "text/html",
-        ".xml": "text/xml",
-        ".zip": "application/zip",
-    }
