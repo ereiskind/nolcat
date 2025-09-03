00ceb4f2f0ee0c5ec59d5bf1a5a974157a0aff15
ereiskind <ereiskind@fsu.edu>
2025-09-03 10:57:48 -0400

    Handle inability to import from `nolcat`

--- tests/create_test_data/convert_SQL_to_parquet.py
+++ tests/create_test_data/convert_SQL_to_parquet.py
@@ -1,47 +1,33 @@
 """This procedure for changing in MySQL to parquet files is inspired by the procedure at https://estuary.dev/blog/mysql-to-parquet/."""
 
-try:
-    from nolcat.nolcat_glue_job import *
-    log.info("`from nolcat.nolcat_glue_job import *` successful.")
-except:
-    try:
-        from ..nolcat.nolcat_glue_job import *
-        log.info("`from ..nolcat.nolcat_glue_job import *` successful.")
-    except:
-        try:
-            from ..nolcat_glue_job import *
-            log.info("`from ..nolcat_glue_job import *` successful.")
-        except:
-            try:
-                from ..nolcat import nolcat_glue_job
-                log.info("`from ..nolcat import nolcat_glue_job` successful.")
-            except:
-                try:
-                    from nolcat import nolcat_glue_job
-                    log.info("`from nolcat import nolcat_glue_job` successful.")
-                except:
-                    try:
-                        from .. import nolcat_glue_job
-                        log.info("`from .. import nolcat_glue_job` successful.")
-                    except:
-                        try:
-                            from nolcat.nolcat.nolcat_glue_job import *
-                            log.info("`from nolcat.nolcat.nolcat_glue_job import *` successful.")
-                        except:
-                            try:
-                                from nolcat.nolcat import nolcat_glue_job
-                                log.info("`from nolcat.nolcat import nolcat_glue_job` successful.")
-                            except:
-                                log.warning("None of the import statements worked")
+from pathlib import Path
+import pandas as pd
 
-log = logging.getLogger(__name__)
+#SECTION: Repeated Content
+# Attempts to import `nolcat.nolcat.nolcat_glue_job.py` failed, so the content needed from that file is repeated here.
+
+secrets = {}
+with open(Path('/nolcat/nolcat/nolcat_secrets.py')) as secrets_file:
+    for line in secrets_file.readlines():
+        divided = line.split(" = '")
+        secrets[divided[0]] = divided[:-3]
+
+SQLALCHEMY_DATABASE_URI = f'mysql://{secrets['Username']}:{secrets['Password']}@{secrets['Host']}:{secrets['Port']}/{secrets['Database']}'
+
+def query_database(query):
+    try:
+        df = pd.read_sql(
+            sql=query,
+            con=SQLALCHEMY_DATABASE_URI,
+        )
+        print(f"The query `{query}` return a dataframe with the following metadata:\n{df.info()}")
+        return df
+    except Exception as error:
+        message = f"Running the query `{query}` raised the error {error}."
+        print(message)
+        return message
 
 
 #SECTION: Break Down SQL Files
 #ToDo: Repeat this section for each production file
-query = "SELECT statistics_source_ID, report_type, report_creation_date FROM COUNTERData GROUP BY statistics_source_ID, report_type, report_creation_date;"
-df = query_database(
-    query=query,
-    engine=db.engine,
-)
-log.info(df)
+df = query_database("SELECT statistics_source_ID, report_type, report_creation_date FROM COUNTERData GROUP BY statistics_source_ID, report_type, report_creation_date;")
