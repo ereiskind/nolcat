95769b3e99bdf3b72f9acae8499c7820fcdc6894
ereiskind <ereiskind@fsu.edu>
2025-09-03 10:28:30 -0400

    Try various imports for 'nolcat_glue_job'

--- tests/create_test_data/convert_SQL_to_parquet.py
+++ tests/create_test_data/convert_SQL_to_parquet.py
@@ -1,6 +1,38 @@
 """This procedure for changing in MySQL to parquet files is inspired by the procedure at https://estuary.dev/blog/mysql-to-parquet/."""
 
-from nolcat.nolcat_glue_job import *
+try:
+    from nolcat.nolcat_glue_job import *
+    log.info("`from nolcat.nolcat_glue_job import *` successful.")
+except:
+    try:
+        from ..nolcat.nolcat_glue_job import *
+        log.info("`from ..nolcat.nolcat_glue_job import *` successful.")
+    except:
+        try:
+            from ..nolcat_glue_job import *
+            log.info("`from ..nolcat_glue_job import *` successful.")
+        except:
+            try:
+                from ..nolcat import nolcat_glue_job
+                log.info("`from ..nolcat import nolcat_glue_job` successful.")
+            except:
+                try:
+                    from nolcat import nolcat_glue_job
+                    log.info("`from nolcat import nolcat_glue_job` successful.")
+                except:
+                    try:
+                        from .. import nolcat_glue_job
+                        log.info("`from .. import nolcat_glue_job` successful.")
+                    except:
+                        try:
+                            from nolcat.nolcat.nolcat_glue_job import *
+                            log.info("`from nolcat.nolcat.nolcat_glue_job import *` successful.")
+                        except:
+                            try:
+                                from nolcat.nolcat import nolcat_glue_job
+                                log.info("`from nolcat.nolcat import nolcat_glue_job` successful.")
+                            except:
+                                log.warning("None of the import statements worked")
 
 log = logging.getLogger(__name__)
 
