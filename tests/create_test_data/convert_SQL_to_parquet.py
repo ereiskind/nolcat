5612d6b68b1e1a523ff0db94564ac378f057ba52
ereiskind <ereiskind@fsu.edu>
2025-09-03 11:10:43 -0400

    Revise unneeded character removal method

--- tests/create_test_data/convert_SQL_to_parquet.py
+++ tests/create_test_data/convert_SQL_to_parquet.py
@@ -10,11 +10,10 @@ secrets = {}
 with open(Path('/nolcat/nolcat/nolcat_secrets.py')) as secrets_file:
     for line in secrets_file.readlines():
         key, value = line.split(" = ")
-        print(value)
-        if value[0] == "'":
-            value == value[1:]
-        if value[-3:] == "'\n":
-            value = value[:-3]
+        print(f"Before: {value}")
+        value = value.replace("'", "")
+        value = value.replace("\n", "")
+        print(f"After: {value}")
         secrets[key] = value
 print(secrets)
 
