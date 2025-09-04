3a705e467c094aa15c0cc2027f02d97b14394437
ereiskind <ereiskind@fsu.edu>
2025-09-04 09:59:53 -0400

    Remove unneeded logging statement

--- tests/create_test_data/convert_SQL_to_parquet.py
+++ tests/create_test_data/convert_SQL_to_parquet.py
@@ -54,7 +54,6 @@ for record in df.iterrows():
     report_type = record[1]['report_type']
     if isinstance(record[1]['report_creation_date'], pd._libs.tslibs.timestamps.Timestamp):
         report_creation_date = str(record[1]['report_creation_date'].isoformat())[:10]
-        print(report_creation_date)
     else:
         report_creation_date = "NULL"
 
