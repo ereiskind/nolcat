fd16f65caa42e0f8f9e1670e15d1e3b1aa25634f
ereiskind <ereiskind@fsu.edu>
2025-10-10 10:35:14 -0400

    Revise temp logging statements

--- tests/test_nolcat_glue_job.py
+++ tests/test_nolcat_glue_job.py
@@ -537,12 +537,13 @@ def test_save_dataframe_to_S3_bucket(tmp_path, dataframe_to_save_to_S3):
     df_from_parquet = pd.read_parquet(download_location)
     #TEST: temp
     try:
-        log.error(f"Dataframe differences:\n{df.reset_index().compare(df_from_parquet)}")
+        log.error(f"`df.reset_index().compare(df_from_parquet)`:\n{df.reset_index().compare(df_from_parquet)}")
     except:
-        log.error(f"`df` columns:\n{df.columns}")
-        log.error(f"`df_from_parquet` columns:\n{df_from_parquet.columns}")
-        log.error(f"`df.reset_index()` index:\n{df.reset_index().index}")
-        log.error(f"`df_from_parquet` index:\n{df_from_parquet.index}")
+        try:
+            log.error(f"`df_from_parquet.compare(df.reset_index())`:\n{df_from_parquet.compare(df.reset_index())}")
+        except:
+            log.error(f"`df.columns == df_from_parquet.columns`: {df.columns == df_from_parquet.columns}")
+            log.error(f"`df.reset_index().index == df_from_parquet.index`: {df.reset_index().index == df_from_parquet.index}")
     #TEST: end temp
     assert_frame_equal(df.reset_index(), df_from_parquet)
 
