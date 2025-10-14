37b221fbf216204d9aa57c933457db122d8cf685
ereiskind <ereiskind@fsu.edu>
2025-10-14 09:35:03 -0400

    Add temp logging for times in file names

--- tests/create_test_data/convert_SQL_to_parquet.py
+++ tests/create_test_data/convert_SQL_to_parquet.py
@@ -97,14 +97,16 @@ for record in df.iterrows():
     statistics_source_ID = record[1]['statistics_source_ID']
     report_type = record[1]['report_type']
     if isinstance(record[1]['report_creation_date'], pd._libs.tslibs.timestamps.Timestamp):
-        report_creation_date = str(record[1]['report_creation_date'].isoformat())[:10]
+        report_creation_date = str(record[1]['report_creation_date'].isoformat())#[:10]
+        print(f"`report_creation_date`: {report_creation_date}")  #TEST: temp
+        print(f"`report_creation_date[:10]`: {report_creation_date[:10]}")  #TEST: temp
     else:
         report_creation_date = "NULL"
 
     if report_creation_date == "NULL":
         query = f"SELECT * FROM COUNTERData WHERE statistics_source_ID={statistics_source_ID} AND report_type='{report_type}' AND report_creation_date IS NULL;"
     else:
-        query = f"SELECT * FROM COUNTERData WHERE statistics_source_ID={statistics_source_ID} AND report_type='{report_type}' AND report_creation_date='{report_creation_date}';"
+        query = f"SELECT * FROM COUNTERData WHERE statistics_source_ID={statistics_source_ID} AND report_type='{report_type}' AND report_creation_date='{report_creation_date[:10]}';"  #TEST: `[:10]` moved from above
     df_to_save = query_database(query)
 
     CSV_file_name = f"{statistics_source_ID}_{report_type}_{report_creation_date}.csv"
