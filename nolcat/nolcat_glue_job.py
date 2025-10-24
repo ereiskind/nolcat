19e829c8b51902236f69db731886d084e9c15287
ereiskind <ereiskind@fsu.edu>
2025-10-24 11:49:51 -0400

    Change form of temp statement stdout

--- nolcat/nolcat_glue_job.py
+++ nolcat/nolcat_glue_job.py
@@ -116,12 +116,12 @@ s3fs.S3FileSystem(profile='PROFILE')
 
 log = logging.getLogger(__name__)
 #TEST: temp
-log.error(f"'nolcat/nolcat/nolcat_glue_job.py' `log` (type {type(log)}):\n{log}")
-log.error(f"'nolcat/nolcat/nolcat_glue_job.py' `log.name` (type {type(log.name)}):\n{log.name}")
-log.error(f"'nolcat/nolcat/nolcat_glue_job.py' `log.parent` (type {type(log.parent)}):\n{log.parent}")
-log.error(f"'nolcat/nolcat/nolcat_glue_job.py' `log.propagate` (type {type(log.propagate)}):\n{log.propagate}")
-log.error(f"'nolcat/nolcat/nolcat_glue_job.py' `log.handlers` (type {type(log.handlers)}):\n{log.handlers}")
-log.error(f"'nolcat/nolcat/nolcat_glue_job.py' `log.getChildren()` (type {type(log.getChildren())}):\n{log.getChildren()}")
+print(f"'nolcat/nolcat/nolcat_glue_job.py' `log` (type {type(log)}):\n{log}")
+print(f"'nolcat/nolcat/nolcat_glue_job.py' `log.name` (type {type(log.name)}):\n{log.name}")
+print(f"'nolcat/nolcat/nolcat_glue_job.py' `log.parent` (type {type(log.parent)}):\n{log.parent}")
+print(f"'nolcat/nolcat/nolcat_glue_job.py' `log.propagate` (type {type(log.propagate)}):\n{log.propagate}")
+print(f"'nolcat/nolcat/nolcat_glue_job.py' `log.handlers` (type {type(log.handlers)}):\n{log.handlers}")
+print(f"'nolcat/nolcat/nolcat_glue_job.py' `log.getChildren()` (type {type(log.getChildren())}):\n{log.getChildren()}")
 #TEST: end temp
 
 
