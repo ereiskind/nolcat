bf366be347400d8da96b62d93525859d5a8c8cf6
ereiskind <ereiskind@fsu.edu>
2025-10-24 12:55:35 -0400

    Add caplog for new logger

--- tests/test_nolcat_glue_job.py
+++ tests/test_nolcat_glue_job.py
@@ -5942,10 +5942,10 @@ def JSON_dicts_with_metadata(request):
         log.error(f"Unable to find a file to delete in `{BUCKET_NAME}/{TEST_COUNTER_FILE_PATH}`")
 
 
-@pytest.mark.skip(reason="Skipping tests known to fail to remove their output from stdout.")  #TEST: temp
 @pytest.mark.slow  # For IR tests
-def test_create_dataframe(tmp_path, JSON_dicts_with_metadata):
+def test_create_dataframe(tmp_path, JSON_dicts_with_metadata, caplog):
     """Tests converting JSONs as Python dicts to dataframes."""
+    caplog.set_level(logging.INFO, logger='nolcat.nolcat_glue_job.ConvertJSONDictToDataframe')
     JSON_report_path, report_type, statistics_source_ID, df_from_fixture = JSON_dicts_with_metadata
     with open(JSON_report_path) as JSON_file:
         dict_from_JSON = json.load(JSON_file)
