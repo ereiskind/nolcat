ce592a218ca58a9cd17f1dc5ff7675601fd7cf23
ereiskind <ereiskind@fsu.edu>
2026-04-16 12:48:44 -0400

    Add credentials check to `fetch_SUSHI_information()`

--- nolcat/models.py
+++ nolcat/models.py
@@ -753,23 +753,36 @@ class StatisticsSources(db.Model):
                         if isinstance(credentials['URL'], Exception):
                             return "How should a returned exception be handled?"  #ToDo: Answer question posed in placeholder
                 
-                    self._log.error(f"`statistics_source_credentials` (type {type(statistics_source_credentials)}):\n{statistics_source_credentials}")  #TEST: temp
-                    if code_of_practice == "5" and statistics_source_credentials['customer_ID'] is not None:
+                    # Some statistics sources use different credentials for different codes of practice. Credentials for codes of practice that are believed to not be the most for the source but still available are placed in fields prepended with "alt_" in the CSV. The default set of credentials in the CSV are tested for the URL returned by the COUNTER Registry; if there's a problem, they're replaced with any available alternate credentials.
+                    if statistics_source_credentials.get('requestor_ID'):
+                        credentials['requestor_id'] = statistics_source_credentials['requestor_ID']
+                    if statistics_source_credentials.get('API_key'):
+                        credentials['api_key'] = statistics_source_credentials['API_key']
+                    if statistics_source_credentials.get('platform'):
+                        credentials['platform'] = statistics_source_credentials['platform']
+                    try:
+                        SUSHI_status_response, messages_to_flash = SUSHICallAndResponse(
+                            self.statistics_source_name,
+                            #self.fetch_SUSHI_information(code_of_practice)['URL'],
+                            "status",
+                            #{key: value for key, value in self.fetch_SUSHI_information(code_of_practice).items() if key != "URL"}
+                        ).make_SUSHI_call(TEST_COUNTER_FILE_PATH)
+                    except (InvalidSUSHIResponseError, DatabaseInteractionErrorWithFlashMessages, S3InteractionErrorWithFlashMessages) as error:
+                        self._log.info(f"Changing to alternate credentials for {self.statistics_source_retrieval_code}.")
                         if statistics_source_credentials.get('alt_customer_ID'):
                             credentials['customer_id'] = statistics_source_credentials['alt_customer_ID']
                         if statistics_source_credentials.get('alt_requestor_ID'):
                             credentials['requestor_id'] = statistics_source_credentials['alt_requestor_ID']
+                        else:
+                            del credentials['requestor_id']
                         if statistics_source_credentials.get('alt_API_key'):
                             credentials['api_key'] = statistics_source_credentials['alt_API_key']
+                        else:
+                            del credentials['api_key']
                         if statistics_source_credentials.get('alt_platform'):
                             credentials['platform'] = statistics_source_credentials['alt_platform']
-                    else:
-                        if statistics_source_credentials.get('requestor_ID'):
-                            credentials['requestor_id'] = statistics_source_credentials['requestor_ID']
-                        if statistics_source_credentials.get('API_key'):
-                            credentials['api_key'] = statistics_source_credentials['API_key']
-                        if statistics_source_credentials.get('platform'):
-                            credentials['platform'] = statistics_source_credentials['platform']
+                        else:
+                            del credentials['platform']  
 
         #Section: Return Data in Requested Format
         if for_API_call:
