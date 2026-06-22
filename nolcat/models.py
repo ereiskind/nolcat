90f74867190442ef2f8dbdb5677312ff5bfb17fd
ereiskind <ereiskind@fsu.edu>
2026-06-22 11:07:25 -0400

    Remove temp logging statement

--- nolcat/models.py
+++ nolcat/models.py
@@ -452,8 +452,7 @@ class FiscalYears(db.Model):
                 notes=record_tuple[9],
             ) for record_tuple in AUCT_objects_to_collect_df.itertuples(name=None)
         ]
-        #TEST: self._log.info(f"The AUCT records of the statistics sources that need their usage collected for FY {self.fiscal_year}:\n{format_list_for_stdout(AUCT_objects_to_collect)}")
-        self._log.error(f"\n\nThe AUCT records of the statistics sources that need their usage collected for FY {self.fiscal_year}:\n{format_list_for_stdout(AUCT_objects_to_collect)}\n\n")  #TEST: temp
+        self._log.info(f"The AUCT records of the statistics sources that need their usage collected for FY {self.fiscal_year}:\n{format_list_for_stdout(AUCT_objects_to_collect)}")
 
         #Section: Collect Usage from Each Statistics Source
         sections_of_UPDATE_statement = []
