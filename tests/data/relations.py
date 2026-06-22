2424abe19e13af430c8587cdb0dec4f444a6d194
ereiskind <ereiskind@fsu.edu>
2026-06-22 11:11:46 -0400

    Null inaccurate note in test data

--- tests/data/relations.py
+++ tests/data/relations.py
@@ -349,7 +349,7 @@ def annualUsageCollectionTracking_relation():
             [True, True, False, False, "Collection complete", "11_2.csv", "This is the first FY with usage statistics"],
             [True, True, False, False, "Collection complete", "11_3.csv", None],
             [True, True, False, False, "Collection complete", "11_4.csv", None],
-            [True, False, False, False, "Collection not started", None, "This is the record for `tests.test_FiscalYears.test_collect_fiscal_year_usage_statistics()`"],
+            [True, False, False, False, "Collection not started", None, None],
         ],
         index=multiindex,
         columns=["usage_is_being_collected", "manual_collection_required", "collection_via_email", "is_COUNTER_compliant", "collection_status", "usage_file_path", "notes"],
